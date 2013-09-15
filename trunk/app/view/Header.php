9c0d7cb9f2a6f30a5be122fd4fd2ff3621ff2cac
mashtree@gmail.com <mashtree@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-09-15 01:55:11 +0000

    css untuk tes, sedikit memberi bentuk biar lebih enak dilihat. tambahan menu utama, menu admin->di setiap file view untuk admin ada tambahan load file menu_admin.php
    catatan : css hanya untuk test saja

--- trunk/app/view/Header.php
+++ trunk/app/view/Header.php
@@ -3,7 +3,7 @@
     <head>
         <title></title>
     <header><b>SCHOLARSHIP INFORMATION MANAGEMENT SYSTEM</b></BR>
-        Bagian Pengembangan Pegawai, Setditjen PBN Jakarta
+        <p>Bagian Pengembangan Pegawai, Setditjen PBN Jakarta</p>
     </header>
     <script src="<?php echo URL; ?>public/js/jquery-2.0.3.min.js"></script>
 
@@ -11,6 +11,7 @@
 <!--    <link href="<?php echo URL; ?>public/css/flick/jquery-ui-1.10.1.custom.css" rel="stylesheet">-->
     <link href="<?php echo URL; ?>public/css/jquery-ui.css" rel="stylesheet">
     <link href="<?php echo URL; ?>public/css/ui.theme.css" rel="stylesheet">
+    <link href="<?php echo URL; ?>public/css/sims.css" rel="stylesheet">
     <hr>
     <script type="text/javascript">
     $(function(){
@@ -19,11 +20,13 @@
     </script>
     </head>
     <body>
-        <div id="menu">
-            <ul>
-                <li><a href="<?php echo URL.'surattugas/datast'?>">Data Surat Tugas</a></li>
-                <li>Data Cuti</li>
-                <li><a href="<?php echo URL.'penerima/datapb';?>">Data Penerima Beasiswa</a></li>
-                <li>Data Master</li>
+        <div class="wrapper">
+            <div class="container">
+            <ul class="menu" rel="sam1">
+                <li><a href="<?php echo URL.'surattugas/datast'?>">Surat Tugas</a></li>
+                <li><a href="">Cuti</a></li>
+                <li><a href="<?php echo URL.'penerima/datapb';?>">Penerima Beasiswa</a></li>
+                <li><a href="">Admin</a></li>
             </ul>
+            </div>
         </div>
