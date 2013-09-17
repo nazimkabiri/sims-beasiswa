e6e1bd454f5e831d9dfd9c0b1c2292806626789b
mashtree@gmail.com <mashtree@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-09-17 03:51:08 +0000

    revisi form penerima beasiswa

--- trunk/app/view/riwayat_tb/penerima_beasiswa.php
+++ trunk/app/view/riwayat_tb/penerima_beasiswa.php
@@ -15,45 +15,22 @@
             }
         ?>
         <label>Surat Tugas</label><select name="st" id="st">
-        </select></br>
-            <option value="1">ST-003</option>
-        <label>Jurusan</label><select name="jur" id="jur">
-            <option value="1">Akuntansi</option>
-        </select></br>
-        <label>Bank</label><select name="bank" id="bank">
-            <option value="1">Bank Mandiri</option>
-        </select></br>
-        <label>Status</label><select name="status" id="status">
-            <option value="1">Belum Lulus</option>
-        </select></br>
-        <label>NIP</label><input type="text" name="nip" id="nip" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_nip():'';?>" size="18"></br>
-        <label>NAMA</label><input type="text" name="nama" id="nama" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_nama():'';?>" size="50"></br>
-        <label>Jenis Kelamin</label><select name="jk" id="jk">
-            <option value="1">Laki-laki</option>
-            <option value="2">Perempuan</option>
-            <option value="3">Abu-abu</option>
-        </select></br>
-        <label>Golongan</label><select name="gol" id="gol">
             <?php 
-                for($i=1;$i<5;$i++){
-                    $gol = 1;
-                    for($j=a;$j<e;$j++){
-                        echo "<option value='$i$gol'>$i $j</option>";
-                        $gol++;
+                if(isset($this->d_st)){
+                    foreach ($this->d_st as $val){
+                        echo "<option value=".$val->get_kd_st().">".$val->get_nomor()." ".Tanggal::tgl_indo($val->get_tgl_st())."</option>";
                     }
                 }
             ?>
         </select></br>
-        <label>Unit Asal</label><input type="text" name="unit_asal" id="unit_asal" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_unit_asal():'';?>" size="50"></br>
+        <label>Bank</label><select name="bank" id="bank">
+            <option value="1">Bank Mandiri</option>
+        </select></br>
         <label>Email</label><input type="text" name="email" id="email" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_email():'';?>" size="30"></br>
         <label>Telepon</label><input type="text" name="telp" id="telp" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_telp():'';?>" size="15"></br>
         <label>Alamat</label><textarea name="alamat" id="alamat"><?php echo isset($this->d_ubah)?$this->d_ubah->get_alamat():'';?></textarea></br>
         <label>No Rekening</label><input type="text" name="no_rek" id="no_rek" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_no_rek():'';?>" size="30"></br>
         <label>Foto</label><input type="file" name="fupload" id="fupload"></br>
-        <label>Tanggal Lapor</label><input type="date" name="tgl_lap" id="tgl_lap" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_tgl_lapor():'';?>" size="10"></br>
-        <label>Nomor SKL</label><input type="text" name="skl" id="skl" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_skl():'';?>" size="20"></br>
-        <label>SPMT</label><input type="text" name="spmt" id="spmt" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_spmt():'';?>" size="20"></br>
-        <label>Judul Skripsi</label><textarea name="skripsi" id="skripsi"><?php echo isset($this->d_ubah)?$this->d_ubah->get_skripsi():'';?></textarea></br>
         <label></label><input type="button" value="BATAL" onClick="location:href('')"><input type="submit" name="<?php echo isset($this->d_ubah)?'sb_upd':'sb_add';?>" value="SIMPAN">
     </form>
 </div>
