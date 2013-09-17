df677b0ed23d6e8c069a5cea78550a7cd911d6e0
nez817 <nez817@gmail.com>
2013-09-17 09:28:54 +0000

    edit data_profil.php, ubah_profil.php, dan style.css

--- app/view/profil/data_profil.php
+++ app/view/profil/data_profil.php
@@ -1,143 +1,281 @@
 <?php //include ('../Header.php');?>
 <!--link href="../../../public/css/bootstrap.css" rel="stylesheet" media="screen"-->
-<div class="container">
-    
-	<h1>Profil Penerima Beasiswa</h1>
-	<h3>Profil Penerima Beasiswa</h3>
+<link href="../../../public/css/style.css" rel="stylesheet" media="screen">
+<body>
+  <div id="wrapper">
+  
+  <!--level1: Profil Penerima Beasiswa-->
+	<h1>PROFIL PENERIMA BEASISWA</h1>
+	<fieldset><legend>Profil Penerima Beasiswa</legend>
+		<div class="foto">
+			<img class="frame" src="..." width="185" height="220">
+			</div>
+		<form action="#" method="post">
+			<label class="isian">NIP :</label>
+			<input class="utama" type="text" id="NIP" name="NIP" disabled />
+			
+			<label class="isian">Nama :</label>
+			<input class="utama" type="text" id="nama" name="nama" disabled />
 
-	<img src="<?php echo URL.'files/'.$this->d_pb->get_foto();?>" width="150" height="150"><br><br>
-<table>
-  <!--form method="POST" action="<?php //$_SERVER['PHP_SELF']; echo URL.'admin/addUniversitas'?>"-->
-	<tr>
-		<td>NIP</td><td>:</td>
-		<td><input type="text" name="NIP" id="NIP" size="50" value="<?php echo isset($this->d_pb)?$this->d_pb->get_nip():'';?>"></td>
-	</tr>
-	<tr>
-		<td>Nama</td><td>:</td>
-		<td><input type="text" name="nama" id="nama" size="50" value="<?php echo isset($this->d_pb)?$this->d_pb->get_nama():'';?>"></td>
-	</tr>
-	<tr>
-		<td>Jenis Kelamin</td><td>:</td>
-		<td><input type="text" name="jeniskelamin" id="jeniskelamin" size="50" value="<?php echo isset($this->d_pb)?$this->d_pb->get_jkel():'';?>"></td>
-	</tr>
-	<tr>
-		<td>Pangkat/Gol</td><td>:</td>
-		<td><input type="text" name="pangkat" id="pangkat" size="50" value="<?php echo isset($this->d_pb)?$this->d_pb->get_gol():'';?>"></td>
-	</tr>
-	<tr>
-		<td>Unit asal</td><td>:</td>
-		<td><input type="text" name="asal" id="asal" size="50" value="<?php echo isset($this->d_pb)?$this->d_pb->get_unit_asal():'';?>"></td>
-	</tr>
-	<tr>
-		<td>Email</td><td>:</td>
-		<td><input type="text" name="email" id="email" size="50" value="<?php echo isset($this->d_pb)?$this->d_pb->get_email():'';?>"></td>
-	</tr>
-	<tr>
-		<td>No. HP</td><td>:</td>
-		<td><input type="text" name="hp" id="hp" size="50" value="<?php echo isset($this->d_pb)?$this->d_pb->get_telp():'';?>"></td>
-	</tr>
-	<tr>
-		<td>Bank Penerima</td><td>:</td>
-		<td><input type="text" name="bank" id="bank" size="10" value="<?php echo isset($this->d_pb)?$this->d_pb->get_bank():'';?>"> 
-		Cabang:<input type="text" name="cabang" id="cabang" size="25"></td>
-	</tr>
-	<tr>
-		<td>No. Rekening</td><td>:</td>
-		<td><input type="text" name="rekening" id="rekening" size="50" value="<?php echo isset($this->d_pb)?$this->d_pb->get_no_rek():'';?>"></td>
-	</tr>
-  <!--/form-->
-</table><br>
-
-<h3>Profil Beasiswa</h3>
-
-<table>
-  <!--form method="POST" action="<?php //$_SERVER['PHP_SELF']; echo URL.'admin/addUniversitas'?>"-->
-	<tr>
-		<td>No. Surat Tugas (ST)</td><td>:</td>
-		<td><input type="text" name="no_st" id="no_st" size="50"></td>
-	</tr>
-	<tr>
-		<td>Tanggal ST</td><td>:</td>
-		<td><input type="text" name="tgl_st" id="tgl_st" size="50"></td>
-	</tr>
-	<tr>
-		<td>Tanggal Mulai ST</td><td>:</td>
-		<td><input type="text" name="tgl_mulai_st" id="tgl_mulai_st" size="50"></td>
-	</tr>
-	<tr>
-		<td>Tanggal Akhir ST</td><td>:</td>
-		<td><input type="text" name="pangkat" id="pangkat" size="50"></td>
-	</tr>
-	<tr>
-		<td>Jenis Beasiswa</td><td>:</td>
-		<td><input type="text" name="jenis_beasiswa" id="jenis_beasiswa" size="50"></td>
-	</tr>
-	<tr>
-		<td>Universitas</td><td>:</td>
-		<td><input type="text" name="universitas" id="universitas" size="50"></td>
-	</tr>
-	<tr>
-		<td>Jurusan/Prodi</td><td>:</td>
-		<td><input type="text" name="jurusan" id="jurusan" size="50"></td>
-	</tr>
-	<tr>
-		<td>Tahun Masuk</td><td>:</td>
-		<td><input type="text" name="th_masuk" id="th_masuk" size="50"></td>
-	</tr>
-	<tr>
-		<td>Status Tugas Belajar</td><td>:</td>
-		<td><input type="text" name="status_tb" id="status_tb" size="50"></td>
-	</tr>
-	<!--row berikut hanya muncul jika status TB: lulus dan/atau tidak lulus -->
-	<tr>
-		<td>Tanggal Akhir TB</td><td>:</td>
-		<td><input type="text" name="status_tb" id="status_tb" size="10">
-		No. SKL: <input type="text" name="status_tb" id="status_tb" size="20">
-		<button>Lihat...</button>
-		</td>
-	</tr>
-	<tr>
-		<td>No. SPMT</td><td>:</td>
-		<td><input type="text" name="status_tb" id="status_tb" size="39">
-		<button>Lihat...</button>
-		</td>
-	</tr>
-  <!--/form-->
-</table>
-
-<h3>Riwayat Pembayaran</h3>
-<table border="1px">
-	<th>No</th>
-	<th>Keterangan Pembayaran</th>
-	<th>Jumlah</th>
-</table>
-
-<h3>Riwayat Perkembangan Tugas Belajar</h3>
-	IPK: <input type="text" name="ipk" id="ipk" size="20">
-	<button>Lihat...</button>
+			<label class="isian">Jenis Kelamin :</label>
+			<input class="utama" type="text" id="jenis_kelamin" name="jenis_kelamin" disabled />
+			
+			<label class="isian">Pangkat/Gol :</label>
+			<input class="utama" type="text" id="pangkat" name="pangkat" disabled />
+			
+			<label class="isian">Unit Asal :</label>
+			<input class="utama" type="text" id="asal" name="asal" disabled />
+			
+			<label class="isian">Alamat :</label>
+			<textarea class="utama" type="text" id="alamat" name="alamat" disabled /></textarea>
+			
+			<label class="isian">Email :</label>
+			<input class="utama" type="email" id="email" name="email" disabled />
+			
+			<label class="isian">No. HP :</label>
+			<input class="utama" type="text" id="hp" name="hp" disabled />
+			
+			<label class="isian">Bank Penerima</label>
+			<input class="utama" type="text" id="bank" name="bank" disabled />
+			
+			<label class="isian">No Rekening</label>
+			<input class="utama" type="text" id="rekening" name="rekening" disabled />
+	</fieldset>
 	
-	<table border="1px">
-		<th>No</th>
-		<th>Keterangan</th>
-		<th>Indeks Prestasi</th>
-		<th>&nbsp;</th>
-	</table>
-	<br>
-	<!--Judul TA sebagai trigger munculnya nama mahasiswa ke List Penerima Biaya Skripsi-->
-	Judul Tugas Akhir: <br>
-		<textarea type="text" name="judul_ta" id="judul_ta" width="150"></textarea><br><br>
-	Permasalahan Tugas Belajar: <br>
-		<textarea type="text" name="masalah_tb" id="masalah_tb" width="150"></textarea><br>
+	<!--level2: Profil Beasiswa & Riwayat Pembayaran-->
+	<div class="kolom1">
+		<fieldset><legend>Profil Beasiswa</legend>
+			<label class="isian">No. Surat Tugas (ST) :</label>
+			<input type="text" id="st" name="st" disabled />
+			
+			<label class="isian">Tanggal ST :</label>
+			<input type="text" id="tgl_st" name="tanggal_st" disabled />
 
-<h3>Riwayat Penerimaan Beasiswa</h3>
-
-	<table border="1px">
-		<th>No</th>
-		<th>No. Surat Tugas</th>
-		<th>Universitas</th>
-		<th>Jurusan/Prodi</th>
-		<th>Tahun Masuk</th>
-		<th>Status TB</th>
-	</table>
-    </div>
+			<label class="isian">Tanggal Mulai ST :</label>
+			<input type="text" id="tgl_mulai_st" name="tgl_mulai_st" disabled />
+			
+			<label class="isian">Tanggal Akhir ST :</label>
+			<input type="text" id="tgl_akhir_st" name="tgl_akhir_st" disabled />
+			
+			<label class="isian">Jenis Beasiswa :</label>
+			<input type="text" id="jenis_beasiswa" name="jenis_beasiswa" disabled />
+			
+			<label class="isian">Universitas :</label>
+			<input type="text" id="universitas" name="universitas" disabled />
+			
+			<label class="isian">Jurusan :</label>
+			<input type="text" id="jurusan" name="jurusan"disabled />
+			
+			<label class="isian">Tahun Masuk :</label>
+			<input type="text" id="th_masuk" name="th_masuk" disabled />
+			
+			<label class="isian">Status Tugas Belajar (TB) :</label>
+			<input type="text" id="status_tb" name="status_tb" disabled />
+			<!--row berikut hanya muncul jika status TB: lulus dan/atau tidak lulus -->
+			
+			<label class="isian">Tanggal Akhir TB :</label>
+			<!--input type="file" id="skl" name="skl" /-->
+			<ul class="inline">
+				<li><input class="unggah" type="text" id="tgl_akhir_tb" disabled /></li>
+				<li><input type="button" value="Lihat" id="fileSKL"/><!--View file SKL-->
+				</li>
+			</ul>
+			
+			<label class="isian">Tanggal Lapor Selesai TB :</label>
+			<ul class="inline">
+				<li><input class="unggah" type="text" id="tgl_lapor" disabled /></li>
+				<li><input type="button" value="Lihat" id="fileSKL"/><!--View file SPMT-->
+				</li>
+			</ul>
+	</fieldset>
 </div>
+<div class="kolom2">
+	<fieldset><legend>Riwayat Pembayaran</legend>
+		
+			<table class="table-bordered zebra scroll">
+				<thead>
+					<th>No</th>
+					<th width="300">Keterangan Pembayaran</th>
+					<th>Jumlah (Rp)</th>
+				</thead>
+				<tbody>
+				<tr>
+					<td>1</td>
+					<td>qwertyui</td>
+					<td align="right">
+						<?php $harga=10000000;
+						echo number_format($harga,2,',','.'); ?>
+					</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>qwertyui</td>
+					<td align="right">
+						<?php $harga=100000;
+						echo number_format($harga,2,',','.'); ?>
+					</td>
+				</tr>
+				</tbody>
+			</table>
+	
+		</fieldset>
+	</div>
+	
+	<!--level3: Riwayat Perkembangan Studi-->
+	<div id="fitur">
+  
+	<fieldset><legend>Riwayat Perkembangan Studi</legend>
+		<div class="kolom5">
+		
+			<label class="isian2">Judul Skripsi :</label>
+			<textarea class="midi" type="text"></textarea>
+			<label class="isian2">Permasalahan Tugas Belajar :</label>
+			<textarea class="midi" type="text" rows="8"></textarea>
+			
+		</div>
+		
+			<label class="isian">IPK :</label>
+			<input type="text" id="IPK" name="IPK" />
+			
+			<label class="isian">Unggah Transkrip:</label>
+			<ul class="inline">
+				<li><input type="file" id="IPK" name="fileipk" style="display: none" onChange="IPKchange();"/>
+				<input class="unggah" type="text" id="namafileipk" disabled /></li>
+				<li><input type="button" value="Pilih..." id="fakeBrowse" onclick="Pilih();"/>
+				</li>
+			</ul>
+			
+			<table class="table-bordered zebra">
+				<thead>
+					<th>No</th>
+					<th>Keterangan</th>
+					<th>IP</th>
+					<th>File</th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td><input class="keterangan" type="text" id="ket" name="ket" /></td>
+						<td><input class="mini" type="text" id="IP" name="IP" /></td>
+						<td><input type="button" value="Pilih..." id="uplod_ip" name="uplod_ip" /></td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td><input class="keterangan" type="text" id="ket" name="ket" /></td>
+						<td><input class="mini" type="text" id="IP" name="IP" /></td>
+						<td><input type="button" value="Pilih..." id="uplod_ip" name="uplod_ip" /></td>
+					</tr>
+				</tbody>
+			</table>
+		
+		</fieldset>
+	</div>
+<!--level4 Riwayat Cuti-->
+<div id="fitur">
+	<fieldset><legend>Riwayat Cuti</legend>
+		
+		<table class="table-bordered zebra" style="display: block">
+				<thead>
+					<th>No</th>
+					<th width="100">No. Surat Cuti</th>
+					<th width="150">Tanggal Surat Cuti</th>
+					<th width="150">Periode Awal Cuti</th>
+					<th width="150">Periode Akhir Cuti</th>
+					<th width="200">Jenis Cuti</th>
+					<th width="100">File</th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>1</td>
+					</tr>
+				</tbody>
+		</table>
+		
+	</fieldset>
+</div> <!--div level 4-->
+
+<!--level5 Riwayat Penerimaan Beasiswa-->
+<div id="fitur">
+	<fieldset><legend>Riwayat Penerimaan Beasiswa</legend>
+		
+		<table class="table-bordered zebra" style="display: block">
+				<thead>
+					<th>No</th>
+					<th width="100">No. Surat Tugas</th>
+					<th width="100">Strata</th>
+					<th width="200">Universitas</th>
+					<th width="100">Jurusan</th>
+					<th width="100">Tahun Masuk</th>
+					<th width="200">Status TB</th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>1</td>
+						<td>1</td>
+						<td>1</td>
+						<td>1</td>
+						<td>1</td>
+						<td>Lulus dengan perpanjangan 2</td>
+					</tr>
+				</tbody>
+		</table>
+		
+	</fieldset>
+</div> <!--div level 4-->
+<input class="sukses" type="submit" value="UBAH" style="font-size: 130%; margin-top:20px"/>
+
+</div> <!--wrapper-->
+
+</body>
+
+<script language="JavaScript" type="text/javascript">
+function HandleBrowseClick()
+{
+    var fileinput = document.getElementById("SKL");
+    fileinput.click();
+}
+function Handlechange()
+{
+var fileinput = document.getElementById("SKL");
+var textinput = document.getElementById("filename");
+textinput.value = fileinput.value;
+}
+
+function BrowseClick()
+{
+    var file = document.getElementById("SPMT");
+    file.click();
+}
+function Change()
+{
+var file = document.getElementById("SPMT");
+var text = document.getElementById("namafile");
+text.value = file.value;
+}
+
+function BrowseFoto()
+{
+    var fotoinput = document.getElementById("foto");
+    fotoinput.click();
+}
+function FotoChange()
+{
+var fotoinput = document.getElementById("foto");
+var foto = document.getElementById("filefoto");
+foto.value = fotoinput.value;
+}
+
+function Pilih()
+{
+    var fileipk = document.getElementById("IPK");
+    fileipk.click();
+}
+function IPKchange()
+{
+var fileipk = document.getElementById("IPK");
+var filenya = document.getElementById("namafileipk");
+filenya.value = fileipk.value;
+}
+</script>
--- app/view/profil/ubah_profil_v2.php
+++ app/view/profil/ubah_profil_v2.php
@@ -5,51 +5,56 @@
   <div id="wrapper">
   
   <!--level1: Profil Penerima Beasiswa-->
+  
 	<h1>PROFIL PENERIMA BEASISWA</h1>
+	
+<div class="kolom1">
 	<fieldset><legend>Profil Penerima Beasiswa</legend>
-		<div class="foto">
-			<img class="frame" src="..." width="185" height="220">
-				<ul class="inline">
-					<li><input type="file" id="foto" name="fotoinput" style="display: none" onChange="FotoChange();"/>
-					<input class="unggah" type="text" id="filefoto" disabled /></li>
-					<li><input type="button" value="Pilih..." id="fakeBrowse" onclick="BrowseFoto();"/>
-					</li>
-				</ul>
-			</div>
+
+
 		<form action="#" method="post">
 			<label class="isian">NIP :</label>
-			<input class="utama" type="text" id="NIP" name="NIP" disabled />
+			<input type="text" id="NIP" name="NIP" disabled />
 			
 			<label class="isian">Nama :</label>
-			<input class="utama" type="text" id="nama" name="nama" disabled />
+			<input type="text" id="nama" name="nama" disabled />
 
 			<label class="isian">Jenis Kelamin :</label>
-			<input class="utama" type="text" id="jenis_kelamin" name="jenis_kelamin" disabled />
+			<input type="text" id="jenis_kelamin" name="jenis_kelamin" disabled />
 			
 			<label class="isian">Pangkat/Gol :</label>
-			<input class="utama" type="text" id="pangkat" name="pangkat" disabled />
+			<input type="text" id="pangkat" name="pangkat" disabled />
 			
 			<label class="isian">Unit Asal :</label>
-			<input class="utama" type="text" id="asal" name="asal" disabled />
+			<input type="text" id="asal" name="asal" disabled />
 			
 			<label class="isian">Alamat :</label>
-			<textarea class="utama" type="text" id="alamat" name="alamat" /></textarea>
+			<textarea type="text" id="alamat" name="alamat" rows="7"/></textarea>
 			
 			<label class="isian">Email :</label>
-			<input class="utama" type="email" id="email" name="email" />
+			<input type="email" id="email" name="email" />
 			
 			<label class="isian">No. HP :</label>
-			<input class="utama" type="text" id="hp" name="hp" />
+			<input type="text" id="hp" name="hp" />
 			
 			<label class="isian">Bank Penerima</label>
-			<input class="utama" type="text" id="bank" name="bank" />
+			<input type="text" id="bank" name="bank" />
 			
 			<label class="isian">No Rekening</label>
-			<input class="utama" type="text" id="rekening" name="rekening" />
+			<input type="text" id="rekening" name="rekening" />
+			
+			<label class="isian">Unggah Foto:</label>
+			<ul class="inline">
+				<li><input type="file" id="foto" name="fotoinput" style="display: none" onChange="FotoChange();"/>
+				<input class="unggah" type="text" id="filefoto" disabled /></li>
+				<li><input type="button" value="Pilih..." id="fakeBrowse" onclick="BrowseFoto();"/>
+				</li>
+			</ul>
+
 	</fieldset>
-	
+</div>	
 	<!--level2: Profil Beasiswa & Riwayat Pembayaran-->
-	<div class="kolom1">
+	<div class="kolom2">
 		<fieldset><legend>Profil Beasiswa</legend>
 			<label class="isian">No. Surat Tugas (ST) :</label>
 			<input type="text" id="st" name="st" disabled />
@@ -109,37 +114,6 @@
 			</ul>
 	</fieldset>
 </div>
-<div class="kolom2">
-	<fieldset><legend>Riwayat Pembayaran</legend>
-		
-			<table class="table-bordered zebra scroll">
-				<thead>
-					<th>No</th>
-					<th width="300">Keterangan Pembayaran</th>
-					<th>Jumlah (Rp)</th>
-				</thead>
-				<tbody>
-				<tr>
-					<td>1</td>
-					<td>qwertyui</td>
-					<td align="right">
-						<?php $harga=10000000;
-						echo number_format($harga,2,',','.'); ?>
-					</td>
-				</tr>
-				<tr>
-					<td>2</td>
-					<td>qwertyui</td>
-					<td align="right">
-						<?php $harga=100000;
-						echo number_format($harga,2,',','.'); ?>
-					</td>
-				</tr>
-				</tbody>
-			</table>
-	
-		</fieldset>
-	</div>
 	
 	<!--level3: Riwayat Perkembangan Studi-->
 	<div id="fitur">
@@ -164,7 +138,7 @@
 				<li><input type="button" value="Pilih..." id="fakeBrowse" onclick="Pilih();"/>
 				</li>
 			</ul>
-			
+			<input type="button" value=" Tambah" style="margin-right: 40px"/>
 			<table class="table-bordered zebra">
 				<thead>
 					<th>No</th>
@@ -190,61 +164,10 @@
 		
 		</fieldset>
 	</div>
-<!--level4 Riwayat Cuti-->
-<div id="fitur">
-	<fieldset><legend>Riwayat Cuti</legend>
-		
-		<table class="table-bordered zebra" style="display: block">
-				<thead>
-					<th>No</th>
-					<th width="100">No. Surat Cuti</th>
-					<th width="150">Tanggal Surat Cuti</th>
-					<th width="150">Periode Awal Cuti</th>
-					<th width="150">Periode Akhir Cuti</th>
-					<th width="200">Jenis Cuti</th>
-					<th width="100">File</th>
-				</thead>
-				<tbody>
-					<tr>
-						<td>1</td>
-						<td>1</td>
-					</tr>
-				</tbody>
-		</table>
-		
-	</fieldset>
-</div> <!--div level 4-->
-
-<!--level5 Riwayat Penerimaan Beasiswa-->
-<div id="fitur">
-	<fieldset><legend>Riwayat Penerimaan Beasiswa</legend>
-		
-		<table class="table-bordered zebra" style="display: block">
-				<thead>
-					<th>No</th>
-					<th width="100">No. Surat Tugas</th>
-					<th width="100">Strata</th>
-					<th width="200">Universitas</th>
-					<th width="100">Jurusan</th>
-					<th width="100">Tahun Masuk</th>
-					<th width="200">Status TB</th>
-				</thead>
-				<tbody>
-					<tr>
-						<td>1</td>
-						<td>1</td>
-						<td>1</td>
-						<td>1</td>
-						<td>1</td>
-						<td>1</td>
-						<td>Lulus dengan perpanjangan 2</td>
-					</tr>
-				</tbody>
-		</table>
-		
-	</fieldset>
-</div> <!--div level 4-->
-<input class="sukses" type="submit" value="SUBMIT" style="font-size: 130%"/>
+<ul class="inline kanan">
+	<li><input class="normal" type="submit" value="BATAL" style="font-size: 130%"/></li>
+	<li><input class="sukses" type="submit" value="SIMPAN" style="font-size: 130%"/></li>
+</form>
 
 </div> <!--wrapper-->
 
