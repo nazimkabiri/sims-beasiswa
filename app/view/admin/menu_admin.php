0ddc4cebd1cbde1775db7b08681b6bb3b9fff755
fromjayakarta <fromjayakarta@gmail.com>
2013-09-17 10:05:48 +0000

    edit menu_admin....

--- app/view/admin/menu_admin.php
+++ app/view/admin/menu_admin.php
@@ -5,7 +5,7 @@
         <li><a href="<?php echo URL;?>admin/addJurusan">JURUSAN</a></li>
         <li><a href="<?php echo URL;?>admin/addStrata">STRATA</a></li>
 		<li><a href="<?php echo URL;?>admin/addPemberi">PEMBERI BEASISWA</a></li>
-		<li><a href="<?php echo URL;?>admin/addPejabat"><h3>PEJABAT</a></li>
+		<li><a href="<?php echo URL;?>admin/addPejabat">PEJABAT</a></li>
         <li><a href="">BANK</a></li>
     </ul>
 </div>
