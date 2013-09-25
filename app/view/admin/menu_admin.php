0d47b8356ac2145c375788c17d1b8f56bc102728
andi.saputra.jakarta <andi.saputra.jakarta@gmail.com>
2013-09-25 04:37:11 +0000

    memperbaiki submenu pada menu admin

--- app/view/admin/menu_admin.php
+++ app/view/admin/menu_admin.php
@@ -4,8 +4,10 @@
         <li><a href="<?php echo URL;?>admin/addFakultas">FAKULTAS</a></li>
         <li><a href="<?php echo URL;?>admin/addJurusan">JURUSAN</a></li>
         <li><a href="<?php echo URL;?>admin/addStrata">STRATA</a></li>
-		<li><a href="<?php echo URL;?>admin/addPemberi">PEMBERI BEASISWA</a></li>
-		<li><a href="<?php echo URL;?>admin/addPejabat">PEJABAT</a></li>
-        <li><a href="">BANK</a></li>
-    </ul>
+        <li><a href="<?php echo URL;?>admin/addPemberi">PEMBERI BEASISWA</a></li>
+        <li><a href="<?php echo URL;?>admin/addST">JENIS SURAT TUGAS</a></li>
+        <li><a href="<?php echo URL;?>admin/addCuti">JENIS SURAT CUTI</a></li>
+	<li><a href="<?php echo URL;?>admin/addBank">BANK</a></li>
+        <li><a href="<?php echo URL;?>admin/addPIC">USER</a></li>
+        <li><a href="<?php echo URL;?>admin/addPejabat">PEJABAT</a></li>
 </div>
