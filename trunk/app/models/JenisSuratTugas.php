915257403fa0b7af53cda358359bb2b4b6d96c3a
andi.saputra.jakarta@gmail.com <andi.saputra.jakarta@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-09-25 06:18:24 +0000

    alhamulillah fungsi crud surat_tugas sudah jadi. YEAY......

--- trunk/app/models/JenisSuratTugas.php
+++ trunk/app/models/JenisSuratTugas.php
@@ -7,13 +7,121 @@
 
 class JenisSuratTugas{
     
+    private $db;
     private $_kd;
     private $_nama;
     private $_keterangan;
+    private $_error;
+    private $_valid = TRUE;
+    private $_table = 'r_jst';
+    public $registry;
     
-    public function __construct() {
-        ;
+    /*
+     * konstruktor
+     */
+    public function __construct($registry = Registry) {
+        $this->db = $registry->db;
+        $this->registry = $registry;
     }
+    
+    /*
+     * mendapatkan data dari tabel jenis surat tugas
+     * @param limit batas default null
+     * return array objek jenis surat tugas
+     */
+    public function get_jst($limit=null,$batas=null){
+        $sql = "SELECT * FROM ".$this->_table." ";
+        if(!is_null($limit) AND !is_null($batas)){
+            $sql .= " LIMIT ".$limit.",".$batas;
+        }
+        $result = $this->db->select($sql);
+        $data=array();
+        foreach ($result as $val){
+            $jst = new $this($this->registry);
+            $jst->set_kode($val['KD_JENIS_ST']);
+            $jst->set_nama($val['NM_JNS_ST']);
+            $jst->set_keterangan($val['KET_JNS_ST']);
+            $data[] = $jst;
+        }
+        return $data;
+    }
+    
+    /*
+     * mendapatkan jenis surat tugas sesuai id
+     * @param objek jenis surat tugas
+     * return objek jenis surat tugas
+     */
+    public function get_jst_by_id($jst = JenisSuratTugas){
+        if(is_null($jst->get_kode())){
+            return false;
+        }
+        $sql = "SELECT * FROM ".$jst->_table." WHERE KD_JENIS_ST=".$jst->get_kode();
+        $result = $this->db->select($sql);
+        foreach ($result as $val){
+            $this->set_kode($val['KD_JENIS_ST']);
+            $this->set_nama($val['NM_JNS_ST']);
+            $this->set_keterangan($val['KET_JNS_ST']);
+        }
+        return $this;
+    }
+    
+    /*
+     * tambah data jenis surat tugas
+     * param array data array key=>value, nama kolom=>data
+     */
+    
+    public function add_jst(){
+        $data = array(
+                'NM_JNS_ST' => $this->get_nama(),
+                'KET_JNS_ST' => $this->get_keterangan()
+            );
+        $this->validate();
+        if(!$this->get_valid()) return false;
+        if(!is_array($data)) return false;
+        $this->db->insert($this->_table,$data);
+    }
+    
+    /*
+     * update jenis surat tugas, id harus di set terlebih dahulu
+     * param data array
+     */
+    
+    public function update_jst(){
+        $data = array(
+                'NM_JNS_ST' => $this->get_nama(),
+                'KET_JNS_ST' => $this->get_keterangan()
+            );
+        $this->validate();
+        if(!$this->get_valid()) return false;
+        if(!is_array($data)) return false;
+        $where = ' KD_JENIS_ST='.$this->get_kode();
+        $this->db->update($this->_table,$data, $where);
+    }
+    
+    /*
+     * hapus jenis surat tugas, id harus di set terlebih dahulu
+     */
+    
+    public function delete_jst(){
+        $where = ' KD_JENIS_ST='.$this->get_kode();
+        $this->db->delete($this->_table,$where);
+    }
+    
+    /*
+     * Validasi untuk inputan
+     */
+    
+    public function validate(){
+        if($this->get_nama()==""){
+            $this->_error .= "Nama Jenis Surat Tugas belum diinput!</br>";
+            $this->_valid = FALSE;
+        }
+    }    
+    
+    
+    /*
+     * setter
+     */
     
     public function set_kode($kode){
         $this->_kd = $kode;
@@ -27,7 +135,18 @@ class JenisSuratTugas{
         $this->_keterangan = $ket;
     }
     
-    public function get_kode(){
+    
+    /*
+     * getter
+     */
+    public function get_kode($where=null){
+        if(!is_null($where)){
+            $sql = "SELECT KD_JENIS_ST '".$this->_table."' WHERE '".$where."'";
+            $result = $this->db->select($sql);
+            foreach ($result as $val){
+                $this->set_kode ($val['KD_JENIS_ST']);
+            }
+        }
         return $this->_kd;
     }
     
@@ -37,6 +156,14 @@ class JenisSuratTugas{
     
     public function get_keterangan(){
         return $this->_keterangan;
+    }
+    
+    public function get_error(){
+        return $this->_error;
+    }
+    
+    public function get_valid(){
+        return $this->_valid;
     }
 
     public function __destruct() {
--- trunk/app/view/admin/surat_tugas.php
+++ trunk/app/view/admin/surat_tugas.php
@@ -1,28 +1,94 @@
-<div id="menu">
-    <select id="menu-admin">
-        <option>-pilih menu-</option>
-    </select>
-</div>
+<?php
+    $this->load('admin/menu_admin');
+?>
 <div id="form">
-    <div id="form-title">DATA JENIS SURAT TUGAS</div>
-    <div id="form-input">
-        <form method="POST" action="<?php $_SERVER['PHP_SELF']; //echo URL.'admin/addUniversitas'?>">
-        <label>Nama</label><input type="text" name="nama" id="nama" size="30"></br>
-        <label>Keterangan</label><input type="text" name="keterangan" id="keterangan" size="50"></br>
-        <label></label><input type="button" onclick="" value="BATAL"><input type="submit" name="add_st" value="SIMPAN">
+    <div><h2>DATA JENIS SURAT TUGAS</h2></div>
+    <div class="kolom3">
+	  <fieldset><legend>Tambah Jenis Surat Tugas</legend>
+		<div id="form-input">
+        <form method="POST" action="<?php 
+            if(isset($this->d_ubah)){
+                echo URL.'admin/updST';
+            }else{
+                $_SERVER['PHP_SELF']; 
+            }
+            
+            ?>">
+        <?php 
+            if(isset($this->d_ubah)){
+                echo "<input type=hidden name='kd_jenis_st' value=".$this->d_ubah->get_kode().">";
+            }
+            
+            if(isset($this->error)){
+                echo "<div class=error>".$this->error."</div>";
+            }
+        ?>
+        <div id="wnama" class="warning_field"></div>
+        <label>Nama</label><input type="text" name="nama" id="nama" size="50" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_nama():'';?>">
+        <div id="wketerangan"></div>
+        <label>Keterangan</label><textarea name="keterangan" id="keterangan" rows="8" type="text"><?php echo isset($this->d_ubah)?$this->d_ubah->get_keterangan():'';?></textarea>
+        <ul class="inline tengah">
+			<li><input class="normal" type="submit" onclick="" value="BATAL"></li>
+			<li><input class="sukses" type="submit" name="<?php echo isset($this->d_ubah)?'upd_st':'add_st';?>" value="SIMPAN" onClick="return cek();"></li>
+		</ul>
         </form>
     </div>
+	</div>
+   </fieldset>
 </div>
-<div id="table">
+<div class="kolom4" id="table">
+	<fieldset><legend>Daftar Jenis Surat Tugas</legend>
     <div id="table-title"></div>
     <div id="table-content">
-        <table>
+        <table class="table-bordered zebra scroll">
             <thead>
                 <th>No</th>
                 <th>Nama</th>
                 <th>Keterangan</th>
                 <th>Aksi</th>
             </thead>
+            <tbody>
+            <?php
+                $no = 1;
+                foreach ($this->data as $val){
+                    echo "<tr>";
+                    echo "<td>$no</td>";
+                    echo "<td>".$val->get_nama()."</td>";
+                    echo "<td>".$val->get_keterangan()."</td>";
+                    echo "<td><a href=".URL."admin/delST/".$val->get_kode().">X</a> | 
+                        <a href=".URL."admin/addST/".$val->get_kode().">...</a></td>";
+                    echo "</tr>";
+                    $no++;
+                }
+            ?>
+            </tbody>
         </table>
     </div>
 </div>
+<script type="text/javascript">
+function cek(){
+    var nama = document.getElementById('nama').value;
+    var keterangan = document.getElementById('keterangan').value;
+    var jml=0;    
+    if(nama==''){
+        var wnama= '<font color="red">Nama Jenis Surat Tugas harus diisi!</font>';
+        $('#wnama').fadeIn(0);
+        $('#wnama').html(wnama);
+        jml++;
+    }
+    
+    if(keterangan==''){
+        var wketerangan= 'Alamat Perguruan Tinggi harus diisi!';
+        $('#wketerangan').fadeIn(0);
+        $('#wketerangan').html(wketerangan);
+        jml++;
+    }
+    
+    if(jml>0){
+        return false
+    }else{
+        return true;
+    }
+    
+}
+</script>
