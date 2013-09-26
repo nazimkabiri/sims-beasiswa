b8c0d0346ad3d99a2a5c0f27e0b24db7d7badfa6
fromjayakarta@gmail.com <fromjayakarta@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-09-26 08:25:06 +0000

    validasi client crud pemberi beasiswa

--- trunk/app/view/admin/edit_pemberi.php
+++ trunk/app/view/admin/edit_pemberi.php
@@ -8,21 +8,21 @@
 			<input type="hidden" name="kd_pemberi" id="kd" size="50" value="<?php echo $this->pemberi->kd_pemberi;?>">
 			
 			<label>Nama</label>
-			<input type="text" name="nama_pemberi" id="nama" size="50" value="<?php echo $this->pemberi->nama_pemberi;?>">
+			<input type="text" name="nama_pemberi" id="nama" size="50" value="<?php echo $this->pemberi->nama_pemberi;?>"><div id="wnama">
         
 			<label>Alamat</label>
-			<textarea type="text" name="alamat_pemberi" id="alamat" rows="8"><?php echo $this->pemberi->alamat_pemberi;?></textarea>
+			<textarea type="text" name="alamat_pemberi" id="alamat" rows="8"><?php echo $this->pemberi->alamat_pemberi;?></textarea><div id="walamat"></div>
         
-			<label>Telepon</label><input type="text" name="telp_pemberi" id="telepon" size="15" value="<?php echo $this->pemberi->telp_pemberi;?>">
+			<label>Telepon</label><input type="text" name="telp_pemberi" id="telepon" size="15" value="<?php echo $this->pemberi->telp_pemberi;?>"><div id="wtelepon"></div>
         
-			<label>PIC</label><input type="text" name="pic_pemberi" id="PIC" size="30" value="<?php echo $this->pemberi->pic_pemberi;?>">
+			<label>PIC</label><input type="text" name="pic_pemberi" id="PIC" size="30" value="<?php echo $this->pemberi->pic_pemberi;?>"><div id="wPIC"></div>
         
-			<label>Telepon PIC</label><input type="text" name="telp_pic_pemberi" id="telp_pic" size="8" value="<?php echo $this->pemberi->telp_pic_pemberi;?>">
+			<label>Telepon PIC</label><input type="text" name="telp_pic_pemberi" id="telp_pic" size="8" value="<?php echo $this->pemberi->telp_pic_pemberi;?>"><div id="wtelp_pic"></div>
         
 			<ul class="inline tengah">
 				<li><input class="normal" type="submit" onclick="window.location.href='<?php echo URL."admin/addPemberi"; ?>'" value="BATAL"></li>
 			
-				<li><input class="sukses" type="submit" name="upd_pemberi" value="SIMPAN"></li>
+				<li><input class="sukses" type="submit" name="upd_pemberi" value="SIMPAN" onclick="return cek();"></li>
 			</ul>
 			
 		</div> <!--end class kiri-->
@@ -63,3 +63,72 @@
 	</fieldset>
 </div>
 </div>
+<script type="text/javascript">
+    
+    $('#nama').keyup(function() {   
+            $('#wnama').fadeOut(0);             
+    });
+	$('#alamat').keyup(function() {   
+            $('#walamat').fadeOut(0);             
+    });
+	$('#telepon').keyup(function() {   
+            $('#wtelepon').fadeOut(0);             
+    });
+	$('#PIC').keyup(function() {   
+            $('#wPIC').fadeOut(0);             
+    });
+	$('#telp_pic').keyup(function() {   
+            $('#wtelp_pic').fadeOut(0);             
+    });
+	
+    function cek(){    
+        var jml = 0;
+        if($('#nama').val()==''){
+            var wnama= 'Nama harus diisi!';
+            $('#wnama').fadeIn(0);
+            $('#wnama').html(wnama);
+			$('#wnama').addClass('error');
+            jml++;
+        }
+    
+        if($('#alamat').val()==''){
+            var walamat= 'Alamat harus diisi!';
+            $('#walamat').fadeIn(0);
+            $('#walamat').html(walamat);
+			$('#walamat').addClass('error');
+            jml++;
+        }
+        
+        if($('#telepon').val()==''){
+            var wtelepon= 'Telepon harus diisi!';
+            $('#wtelepon').fadeIn(0);
+            $('#wtelepon').html(wtelepon);
+			$('#wtelepon').addClass('error');
+            jml++;
+        }
+    
+        if($('#PIC').val()==''){
+            var wPIC= 'PIC harus diisi!';
+            $('#wPIC').fadeIn(0);
+            $('#wPIC').html(wPIC);
+			$('#wPIC').addClass('error');
+            jml++;
+        }
+        
+        if($('#telp_pic').val()==''){
+            var wtelp_pic= 'Telepon PIC harus diisi!';
+            $('#wtelp_pic').fadeIn(0);
+            $('#wtelp_pic').html(wtelp_pic);
+			$('#wtelp_pic').addClass('error');
+            jml++;
+        }
+    
+    
+    
+        if(jml>0){
+            //alert('Isian form belum lengkap');
+            return false;
+        }
+    }
+    
+</script>
