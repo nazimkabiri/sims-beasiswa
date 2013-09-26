7430a3f20b08208365de103ee923cc4277812610
fromjayakarta <fromjayakarta@gmail.com>
2013-09-26 08:08:45 +0000

    validasi client crud strata

--- app/view/admin/edit_strata.php
+++ app/view/admin/edit_strata.php
@@ -8,14 +8,14 @@
         <div class="kiri">
 		<input type="hidden" name="kd_strata" id="kd" value="<?php echo $this->strata->kd_strata;?>">
         
-		<label>Kode</label><input type="text" name="kode_strata" id="kode" size="8" value="<?php echo $this->strata->kode_strata;?>">
+		<label>Kode</label><input type="text" name="kode_strata" id="kode" size="8" value="<?php echo $this->strata->kode_strata;?>"><div id="wkode_strata"></div>
         
-		<label>Nama</label><input type="text" name="nama_strata" id="nama" size="50" value="<?php echo $this->strata->nama_strata;?>">
+		<label>Nama</label><input type="text" name="nama_strata" id="nama" size="50" value="<?php echo $this->strata->nama_strata;?>"><div id="wnama_strata"></div>
         
 		<ul class="inline tengah">
 			<li><input class="normal" type="submit" onclick="window.location.href='<?php echo URL."admin/addStrata"; ?>'" value="BATAL"></li>
 			
-			<li><input class="sukses" type="submit" name="upd_strata" value="SIMPAN"></li>
+			<li><input class="sukses" type="submit" name="upd_strata" value="SIMPAN" onClick="return cek();"></li>
 		</ul>
 		
         </div> <!--end class kiri-->
@@ -50,3 +50,38 @@
 	</fieldset>
 </div>
 </div>
+<script type="text/javascript">
+	
+	$('#kode').keyup(function() {   
+            $('#wkode_strata').fadeOut(0);             
+    });
+	
+	$('#nama').keyup(function() {   
+            $('#wnama_strata').fadeOut(0);             
+    });
+	
+    function cek(){    
+        var jml = 0;
+        if($('#kode').val()==''){
+            var wkode_strata= 'Kode harus diisi!';
+            $('#wkode_strata').fadeIn(0);
+            $('#wkode_strata').html(wkode_strata);
+			$('#wkode_strata').addClass('error');
+            jml++;
+        }
+    
+        if($('#nama').val()==''){
+            var wnama_strata= 'Nama harus diisi!';
+            $('#wnama_strata').fadeIn(0);
+            $('#wnama_strata').html(wnama_strata);
+			$('#wnama_strata').addClass('error');
+            jml++;
+        }
+    
+        if(jml>0){
+            //alert('Isian form belum lengkap');
+            return false;
+        }
+    }
+    
+</script>
--- app/view/admin/strata.php
+++ app/view/admin/strata.php
@@ -5,11 +5,11 @@
 		   <fieldset><legend>Tambah Strata</legend>
 			<div id="form-input" class="kiri" >
             <form method="POST" action="<?php /* $_SERVER['PHP_SELF']; */ echo URL . 'admin/addStrata' ?>">
-                <label>Kode</label><input type="text" name="kode_strata" id="kode" size="8">
-                <label>Strata</label><input type="text" name="nama_strata" id="nama" size="50">
+                <label>Kode</label><input type="text" name="kode_strata" id="kode" size="8"><div id="wkode_strata"></div>
+                <label>Strata</label><input type="text" name="nama_strata" id="nama" size="50"><div id="wnama_strata"></div>
               <ul class="inline tengah">  
 				<li><input class="normal" type="submit" value="BATAL"></li>
-				<li><input class="sukses" type="submit" name="add_strata" value="SIMPAN"></li>
+				<li><input class="sukses" type="submit" name="add_strata" value="SIMPAN" onClick="return cek();"></li>
 			  </ul>
             </form>
         </div>
@@ -34,7 +34,7 @@
                         <td><?php echo $strata->kode_strata; ?></td>
                         <td><?php echo $strata->nama_strata; ?></td>
                         <td align="center">
-                            <?php echo "<a href=" . URL . "admin/delStrata/" . $strata->kd_strata . ">X</a> | 
+                            <?php echo "<a href=" . URL . "admin/delStrata/" . $strata->kd_strata . " onclick=\"return del()\">X</a> | 
                     <a href=" . URL . "admin/editStrata/" . $strata->kd_strata . ">...</a>" ?>
                         </td>
                     </tr>
@@ -46,4 +46,44 @@
    </fieldset>
 </div>
 </div>
-
+<script type="text/javascript">
+    
+    function del(){
+        if(confirm('Apakah Anda yakin akan menghapus data ini?'))
+            return true;
+        else return false
+    }
+	
+	$('#kode').keyup(function() {   
+            $('#wkode_strata').fadeOut(0);             
+    });
+	
+	$('#nama').keyup(function() {   
+            $('#wnama_strata').fadeOut(0);             
+    });
+	
+    function cek(){    
+        var jml = 0;		
+        if($('#kode').val()==''){
+            var wkode_strata= 'Kode harus diisi!';
+            $('#wkode_strata').fadeIn(0);
+            $('#wkode_strata').html(wkode_strata);
+			$('#wkode_strata').addClass('error');
+            jml++;
+        }
+    
+        if($('#nama').val()==''){
+            var wnama_strata= 'Nama harus diisi!';
+            $('#wnama_strata').fadeIn(0);
+            $('#wnama_strata').html(wnama_strata);
+			$('#wnama_strata').addClass('error');
+            jml++;
+        }
+    
+        if(jml>0){
+            //alert('Isian form belum lengkap');
+            return false;
+        }
+    }
+    
+</script>
