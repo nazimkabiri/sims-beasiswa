bd95618d012b2ef6bf586ca07b6d4e21ebf78ef9
asuracentaurix <asuracentaurix@gmail.com>
2013-09-26 15:29:30 +0000

    update view edit user

--- app/view/admin/edit_user.php
+++ app/view/admin/edit_user.php
@@ -1,28 +1,62 @@
 <div id="top">
     <h2>UBAH DATA USER</h2>
-	<div class="kolom3">
-	  <fieldset><legend>Ubah User</legend>
-		
-		<div id="form-input">           
-        <form method="POST" enctype="multipart/form-data" action=" <?php echo URL.'Admin/updateUser' ?>">
-            
-			<div class="kiri">
-			<input type="hidden" name="id" id="id" value="<?php echo $this->data->get_id(); ?>" size="30">
-            <label>NIP</label><input type="text" name="nip" id="nama" value="<?php echo $this->data->get_nip(); ?>" size="30"/>
-            <label>Nama</label><input type="text" name="nama" id="nama" value="<?php echo $this->data->get_nmUser(); ?>" size="30"/>
-            <label>Password</label><input type="text" name="pass" id="nama" value="<?php echo $this->data->get_pass(); ?>" size="30"/>
-            <label>Akses</label><input type="text" name="akses" id="nama" value="<?php echo $this->data->get_akses(); ?>" size="30"/>
-            <label>Upload Foto</label><input type="file" name="foto" id="nama" value="" size="30"/>
-				<ul class="inline tengah">
-					<li><input class="biru" type="button" onclick="" value="BATAL"></li>
-					<li><input class="sukses" type="submit" name="submit" value="SIMPAN"></li>
-				</ul>
-			</div>
-			
-        </form>
+    <div class="kolom3">
+        <fieldset><legend>Ubah User</legend>
+
+            <div id="form-input">           
+                <form method="POST" enctype="multipart/form-data" action=" <?php echo URL . 'Admin/updateUser' ?>">
+
+                    <div class="kiri">
+                        <input type="hidden" name="id" id="id" value="<?php echo $this->data->get_id(); ?>" size="30">
+                        <label>NIP</label><input type="text" name="nip" id="nama" value="<?php echo $this->data->get_nip(); ?>" size="30"/>
+                        <label>Nama</label><input type="text" name="nama" id="nama" value="<?php echo $this->data->get_nmUser(); ?>" size="30"/>
+                        <label>Password</label><input type="text" name="pass" id="nama" value="<?php echo $this->data->get_pass(); ?>" size="30"/>
+                        <label>Akses</label><input type="text" name="akses" id="nama" value="<?php echo $this->data->get_akses(); ?>" size="30"/>
+                        <label>Upload Foto</label><input type="file" name="foto" id="nama" value="" size="30"/>
+                        <ul class="inline tengah">
+                            <li><input class="normal" type="submit" onclick="" value="BATAL"></li>
+                            <li><input class="sukses" type="submit" name="submit" value="SIMPAN"></li>
+                        </ul>
+                    </div>
+
+                </form>
+            </div>
+        </fieldset>
     </div>
-	</fieldset>
-</div>
+    <div class="kolom4" id="table">
+        <fieldset><legend>Daftar User</legend>
+            <div id="table-title"></div>
+            <div id="table-content">
+                <table class="table-bordered zebra scroll">
+                    <thead>
+                    <th width="20%">NIP</th>
+                    <th width="20%">Nama</th>
+                    <th width="20%">Pass</th>
+                    <th width="10%">Akses</th>
+                    <th width="20%">Foto</th>
+                    <th width="5%">Aksi</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($this->data2 as $value) {
+                            echo '<tr>';
+                            echo '<td>' . $value->get_nip() . '</td>';
+                            echo '<td>' . $value->get_nmUser() . '</td>';
+                            echo '<td>' . $value->get_pass() . '</td>';
+                            echo '<td>' . $value->get_akses() . '</td>';
+                            echo '<td>' . $value->get_foto() . '</td>';
+                            echo '<td>
+                        <a href="' . URL . 'Admin/deleteUser/' . $value->get_id() . '">X</a>
+			<a href="' . URL . 'Admin/editUser/' . $value->get_id() . '">...</a>
+                        </td>';
+                            echo '</tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+    </div>
+
 </div>
 
 
