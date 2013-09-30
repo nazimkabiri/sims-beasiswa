9082ae606d3a6e48f627a02ac43ef919cbd8eecf
asuracentaurix@gmail.com <asuracentaurix@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-09-30 01:06:06 +0000

    nambahin JS

--- trunk/app/view/admin/list_bank.php
+++ trunk/app/view/admin/list_bank.php
@@ -1,59 +1,68 @@
 <div id="top">
     <h2>DATA BANK</h2>
-	<div class="kolom3">
-	  <fieldset><legend>Tambah Data Bank</legend>
-		<div id="form-input">
-			<form method="POST" action="<?php echo URL.'Admin/addBank' ?> ">
-				<div class="kiri">
-				<label>Nama</label><input type="text" name="nama" id="nama" size="30">
-				<label>Keterangan</label><input type="text" name="keterangan" id="keterangan" size="50">
-				
-				<ul class="inline tengah">
-					<li><input class="normal" type="submit" onclick="" value="BATAL"></li>
-					<li><input class="sukses" type="submit" name="submit" value="SIMPAN"></li>
-				</ul>
-				</div>
-			</form>
-		</div>
-		</fieldset>
-	</div>
-<div class="kolom4" id="table">
-	  <fieldset><legend>Daftar Data Bank</legend>
-			<div id="table-title"></div>
-			<div id="table-content">
-			<table class="table-bordered zebra scroll">
-				<thead>
-					<th>No</th>
-					<th width="200">Nama Bank</th>
-					<th width="400">Keterangan</th>
-					<th width="30">Aksi</th>
-				</thead>
-            <tbody>
-                <?php
-                $i = 1;
-                foreach ($this->data as $value) {
-                    echo '<tr>';
-                    echo '<td>' . $i . '</td>';
-                    echo '<td>' . $value->get_nama() . '</td>';
-                    echo '<td>' . $value->get_keterangan() . '</td>';
-                    echo
-                    '<td>
+    <div class="kolom3">
+        <fieldset><legend>Tambah Data Bank</legend>
+            <div id="form-input">
+                <form method="POST" action="<?php echo URL . 'Admin/addBank' ?>" name="myform" onsubmit="return(validate())">
+                    <div class="kiri">
+                        <label>Nama</label><input type="text" name="nama" id="nama" size="30"><div id="warningnama"></div>
+                        <label>Keterangan</label><input type="text" name="keterangan" id="keterangan" size="50">
+
+                        <ul class="inline tengah">
+                            <li><input class="normal" type="submit" onclick="" value="BATAL"></li>
+                            <li><input class="sukses" type="submit" name="submit" value="SIMPAN"></li>
+                        </ul>
+                    </div>
+                </form>
+            </div>
+        </fieldset>
+    </div>
+    <div class="kolom4" id="table">
+        <fieldset><legend>Daftar Data Bank</legend>
+            <div id="table-title"></div>
+            <div id="table-content">
+                <table class="table-bordered zebra scroll">
+                    <thead>
+                    <th>No</th>
+                    <th width="200">Nama Bank</th>
+                    <th width="400">Keterangan</th>
+                    <th width="30">Aksi</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $i = 1;
+                        foreach ($this->data as $value) {
+                            echo '<tr>';
+                            echo '<td>' . $i . '</td>';
+                            echo '<td>' . $value->get_nama() . '</td>';
+                            echo '<td>' . $value->get_keterangan() . '</td>';
+                            echo
+                            '<td>
                         <a href="' . URL . 'Admin/deleteBank/' . $value->get_id() . '">X</a>
 			<a href="' . URL . 'Admin/editBank/' . $value->get_id() . '">...</a>
                     </td>';
-                    echo '</tr>';
-                    $i++;
-                }
-                ?>            
-            </tbody>
-        </table>
+                            echo '</tr>';
+                            $i++;
+                        }
+                        ?>            
+                    </tbody>
+                </table>
+            </div>
+        </fieldset>
     </div>
-	</fieldset>
-</div>
 </div>
 
 <script type="text/javascript">
     function validate (){
-        
+        if( document.myform.nama.value == "" )
+        {
+          var warning = 'nama bank harus diisi';
+          $('#warningnama').fadeIn(0);
+          $('#warningnama').html(warning);
+          $('#warningnama').addClass('error');
+        }
+       
     }
+    //-->
 </script>
+
