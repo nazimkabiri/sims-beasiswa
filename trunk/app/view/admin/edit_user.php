7f9d362e01c55246265a086805194e04b26ca5d9
asuracentaurix@gmail.com <asuracentaurix@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-10-01 07:33:16 +0000

    update javascript

--- trunk/app/view/admin/edit_user.php
+++ trunk/app/view/admin/edit_user.php
@@ -4,16 +4,16 @@
         <fieldset><legend>Ubah User</legend>
 
             <div id="form-input">           
-                <form method="POST" enctype="multipart/form-data" action=" <?php echo URL . 'Admin/updateUser' ?>">
+                <form method="POST" enctype="multipart/form-data" name="myform" onsubmit="return (validate());" action=" <?php echo URL . 'Admin/updateUser' ?>">
 
                     <div class="kiri">
                         <input type="hidden" name="id" id="id" value="<?php echo $this->data->get_id(); ?>" size="30">
-                        <label>NIP</label><input type="text" name="nip" id="nama" value="<?php echo $this->data->get_nip(); ?>" size="30"/>
-                        <label>Nama</label><input type="text" name="nama" id="nama" value="<?php echo $this->data->get_nmUser(); ?>" size="30"/>
-                        <label>Password</label><input type="password" name="pass" id="nama" value="" size="30"/>
-                        <label>Confirm Password</label><input type="password" name="cpass" id="nama" value="" size="30"/>
-                        <label>Akses</label><input type="text" name="akses" id="nama" value="<?php echo $this->data->get_akses(); ?>" size="30"/>
-                        <label>Upload Foto</label><input type="file" name="foto" id="nama" value="" size="30"/>
+                        <label>NIP</label><input type="text" name="nip" id="nip" value="<?php echo $this->data->get_nip(); ?>" size="30"/><div id="warningnip" ></div>
+                        <label>Nama</label><input type="text" name="nama" id="nama" value="<?php echo $this->data->get_nmUser(); ?>" size="30"/><div id="warningnama" ></div>
+                        <label>Password</label><input type="password" name="pass" id="pass" value="" size="30"/><div id="warningpass" ></div>
+                        <label>Confirm Password</label><input type="password" name="cpass" id="cpass" value="" size="30"/>
+                        <label>Akses</label><input type="text" name="akses" id="akses" value="<?php echo $this->data->get_akses(); ?>" size="30"/><div id="warningakses" ></div>
+                        <label>Upload Foto</label><input type="file" name="foto" id="foto" value="" size="30"/>
                         <ul class="inline tengah">
                             <li><input class="normal" type="reset" onclick="" value="BATAL"></li>
                             <li><input class="sukses" type="submit" name="submit" value="SIMPAN" onclick=""></li>
@@ -57,5 +57,53 @@
     </div>
 
 </div>
-
+<script type="text/javascript">
+    function validate(){
+        var jml=0;
+        
+        if (document.myform.nip.value==""){
+            var warning1 = 'nip harus diisi';
+            $('#warningnip').fadeIn(0);
+            $('#warningnip').html(warning1);
+            $('#warningnip').addClass('error');        
+            jml++;
+        }
+        if (document.myform.nama.value==""){
+            var warning2 = 'nama harus diisi';
+            $('#warningnama').fadeIn(0);
+            $('#warningnama').html(warning2);
+            $('#warningnama').addClass('error');
+            jml++;
+        }
+    
+        if (document.myform.akses.value==""){
+            var warning5 = 'akses belum diisi';
+            $('#warningakses').fadeIn(0);
+            $('#warningakses').html(warning5);
+            $('#warningakses').addClass('error');
+            jml++;
+        }
+        if(document.myform.pass.value==""){
+          var warning3 = 'password harus diisi';
+          $('#warningpass').fadeIn(0);
+          $('#warningpass').html(warning3);
+          $('#warningpass').addClass('error');
+          jml++;
+        } else {
+            if(document.myform.pass.value!==document.myform.cpass.value){
+                var warning4 = 'password tidak sama dengan confirm password nya';
+                $('#warningpass').fadeIn(0);
+                $('#warningpass').html(warning4);
+                $('#warningpass').addClass('error');
+                jml++;
+            }
+        }
+        
+        if (jml>0){
+            return false;
+        }
+        
+    }
+    
+</script>
 
--- trunk/app/view/admin/list_user.php
+++ trunk/app/view/admin/list_user.php
@@ -4,18 +4,16 @@
         <fieldset><legend>Tambah User</legend>
             <div id="form-input">           
 
-                <form method="POST" enctype="multipart/form-data" action=" <?php echo URL . 'Admin/addUser' ?>">
+                <form method="POST" enctype="multipart/form-data" name="myform" onsubmit="return (validate());" action=" <?php echo URL . 'Admin/addUser' ?>">
 
                     <div class= "kiri">
                         <input type="hidden" name="id" id="id" value="" size="30">
-
-                        <label>NIP</label><input type="text" name="nip" id="nama" value="" size="30"/>
-
-                        <label>Nama</label><input type="text" name="nama" id="nama" value="" size="30"/>
-                        <label>PASS</label><input type="password" name="pass" id="nama" value="" size="30"/>
-                        <label>CONFIRM PASS</label><input type="password" name="cpass" id="nama" value="" size="30"/>
-                        <label>AKSES</label><input type="text" name="akses" id="nama" value="" size="30"/>
-                        <label>Upload Foto</label><input type="file" name="foto" id="nama" value="" size="30"/>
+                        <label>NIP</label><input type="text" name="nip" id="nip" value="" size="30"/><div id="warningnip" ></div>
+                        <label>Nama</label><input type="text" name="nama" id="nama" value="" size="30"/><div id="warningnama"></div>
+                        <label>PASS</label><input type="password" name="pass" id="pass" value="" size="30"/><div id="warningpass"></div>
+                        <label>CONFIRM PASS</label><input type="password" name="cpass" id="cpass" value="" size="30"/>
+                        <label>AKSES</label><input type="text" name="akses" id="akses" value="" size="30"/><div id="warningakses"></div>
+                        <label>Upload Foto</label><input type="file" name="foto" id="upload" value="" size="30"/>
 
                         <ul class="inline tengah">
                             <li><input class= "normal" type="reset" onclick="" value="BATAL"></li>
@@ -64,3 +62,52 @@
             </div>
     </div>
 </div>
+<script type="text/javascript">
+    function validate(){
+        var jml=0;
+        
+        if (document.myform.nip.value==""){
+            var warning1 = 'nip harus diisi';
+            $('#warningnip').fadeIn(0);
+            $('#warningnip').html(warning1);
+            $('#warningnip').addClass('error');        
+            jml++;
+        }
+        if (document.myform.nama.value==""){
+            var warning2 = 'nama harus diisi';
+            $('#warningnama').fadeIn(0);
+            $('#warningnama').html(warning2);
+            $('#warningnama').addClass('error');
+            jml++;
+        }
+    
+        if (document.myform.akses.value==""){
+            var warning5 = 'akses belum diisi';
+            $('#warningakses').fadeIn(0);
+            $('#warningakses').html(warning5);
+            $('#warningakses').addClass('error');
+            jml++;
+        }
+        if(document.myform.pass.value==""){
+          var warning3 = 'password harus diisi';
+          $('#warningpass').fadeIn(0);
+          $('#warningpass').html(warning3);
+          $('#warningpass').addClass('error');
+          jml++;
+        } else {
+            if(document.myform.pass.value!==document.myform.cpass.value){
+                var warning4 = 'password tidak sama dengan confirm password nya';
+                $('#warningpass').fadeIn(0);
+                $('#warningpass').html(warning4);
+                $('#warningpass').addClass('error');
+                jml++;
+            }
+        }
+        
+        if (jml>0){
+            return false;
+        }
+        
+    }
+    
+</script>
