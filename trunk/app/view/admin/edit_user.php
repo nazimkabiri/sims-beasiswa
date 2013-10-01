61e195d216dfb5706a64965f34dc3be428ccec1c
asuracentaurix@gmail.com <asuracentaurix@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-10-01 04:08:15 +0000

    menambahkan field confirm password

--- trunk/app/view/admin/edit_user.php
+++ trunk/app/view/admin/edit_user.php
@@ -10,12 +10,13 @@
                         <input type="hidden" name="id" id="id" value="<?php echo $this->data->get_id(); ?>" size="30">
                         <label>NIP</label><input type="text" name="nip" id="nama" value="<?php echo $this->data->get_nip(); ?>" size="30"/>
                         <label>Nama</label><input type="text" name="nama" id="nama" value="<?php echo $this->data->get_nmUser(); ?>" size="30"/>
-                        <label>Password</label><input type="text" name="pass" id="nama" value="<?php echo $this->data->get_pass(); ?>" size="30"/>
+                        <label>Password</label><input type="password" name="pass" id="nama" value="" size="30"/>
+                        <label>Confirm Password</label><input type="password" name="cpass" id="nama" value="" size="30"/>
                         <label>Akses</label><input type="text" name="akses" id="nama" value="<?php echo $this->data->get_akses(); ?>" size="30"/>
                         <label>Upload Foto</label><input type="file" name="foto" id="nama" value="" size="30"/>
                         <ul class="inline tengah">
-                            <li><input class="normal" type="submit" onclick="" value="BATAL"></li>
-                            <li><input class="sukses" type="submit" name="submit" value="SIMPAN"></li>
+                            <li><input class="normal" type="reset" onclick="" value="BATAL"></li>
+                            <li><input class="sukses" type="submit" name="submit" value="SIMPAN" onclick=""></li>
                         </ul>
                     </div>
 
