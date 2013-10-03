5acdebae0b454be119d5ee093b3bd1b2d3e930c5
asuracentaurix <asuracentaurix@gmail.com>
2013-10-03 08:11:21 +0000

    upp

--- app/models/User.php
+++ app/models/User.php
@@ -16,25 +16,20 @@ class User {
     private $_pass;
     private $_akses;
     private $_foto;
-
-    /*
-     * Konstruktor
-     */
+    private $_table2 = "r_user";
 
     public function __construct($registry) {
         $this->registry = $registry;
         $this->_db = new Database();
     }
 
-    /*
-     * Mendapatkan semua user
-     */
-
     public function get_user($level = FALSE) {
 
-        $sql = "SELECT * FROM " . $this->_table . "";
+        $sql = "SELECT ".$this->_table.".KD_USER, ".$this->_table.".NIP_USER, ".$this->_table.".NM_USER, ".$this->_table.".PASS_USER,
+                ".$this->_table2.".nama as AKSES_USER, ".$this->_table.".FOTO_USER FROM ".$this->_table." LEFT JOIN ".$this->_table2."
+                on ".$this->_table.".AKSES_USER = ".$this->_table2.".id ";
         if ($level) {
-            $sql .=" WHERE AKSES_USER=2";
+            $sql .=" WHERE AKSES_USER= 'user'";
         }
 
         $result = $this->_db->select($sql);
@@ -54,36 +49,28 @@ class User {
         return $data;
     }
 
-    /*
-     * Mendapatkan user berdasarkan id_user
-     * @param id_user
-     */
-
     public function getUser_id($KD_USER) {
 
-        $sql = "SELECT * FROM " . $this->_table . " WHERE KD_USER = " . $KD_USER;
+        $sql = "SELECT * FROM " . $this->_table . " WHERE KD_USER = " . $KD_USER . "";
+
         $result = $this->_db->select($sql);
-//        $data = array();
+
+        $data = array();
 
         foreach ($result as $value) {
-//            $user = new User($this->registry);
-            $this->set_id($value['KD_USER']);
-            $this->set_nip($value['NIP_USER']);
-            $this->set_nmUser($value['NM_USER']);
-            $this->set_pass($value['PASS_USER']);
-            $this->set_akses($value['AKSES_USER']);
-            $this->set_foto($value['FOTO_USER']);
+            $user = new User($registry);
+            $user->set_id($value['KD_USER']);
+            $user->set_nip($value['NIP_USER']);
+            $user->set_nmUser($value['NM_USER']);
+            $user->set_pass($value['PASS_USER']);
+            $user->set_akses($value['AKSES_USER']);
+            $user->set_foto($value['FOTO_USER']);
 
-//            $data = $user;
+            $data = $user;
         }
 
-        return $this;
+        return $data;
     }
-
-    /*
-     * Mengecek apakah nip user telah terdaftar di database
-     * @param nip
-     */
 
     public function check_user_nip($nip) {
 
@@ -96,10 +83,6 @@ class User {
 //        var_dump($count);
         return $count;
     }
-
-    /*
-     * menambahkan user pada database
-     */
 
     public function addUser(User $user) {
 
@@ -118,84 +101,80 @@ class User {
         }
     }
 
-    /*
-     * merubah user pada database
-     */
-
     public function updateUser(User $user) {
+      
+            $where = "KD_USER = " . $user->get_id();
 
-        $where = "KD_USER = " . $user->get_id();
-
-        $data = array(
-            'NIP_USER' => $user->get_nip(),
-            'NM_USER' => $user->get_nmUser(),
-            'PASS_USER' => Hash::create('sha1', $user->get_pass(), HASH_SALT_KEY),
-            'AKSES_USER' => $user->get_akses(),
-            'FOTO_USER' => $user->get_foto()
-        );
-        $this->_db->update($this->_table, $data, $where);
+            $data = array(
+                'NIP_USER' => $user->get_nip(),
+                'NM_USER' => $user->get_nmUser(),
+                'PASS_USER' => Hash::create('sha1', $user->get_pass(), HASH_SALT_KEY),
+                'AKSES_USER' => $user->get_akses(),
+                'FOTO_USER' => $user->get_foto()
+            );
+            $this->_db->update($this->_table, $data, $where);
+       
     }
+    public function updateUser_withoutpass (User $user){
+            $where = "KD_USER = " . $user->get_id();
 
-    /*
-     * mnghapus user pada database
-     */
-
+//            var_dump($user->get_pass());
+            $data = array(
+                'NIP_USER' => $user->get_nip(),
+                'NM_USER' => $user->get_nmUser(),
+                'AKSES_USER' => $user->get_akses(),
+                'FOTO_USER' => $user->get_foto()
+            );
+            $this->_db->update($this->_table, $data, $where);
+    }
     public function delUser($id) {
         $where = "KD_USER=" . $id;
 
         $this->_db->delete($this->_table, $where);
     }
 
-    /*
-     * getter
-     */
-
     public function get_id() {
         return $this->_id;
+    }
+
+    public function set_id($id) {
+        $this->_id = $id;
     }
 
     public function get_nip() {
         return $this->_nip;
     }
 
-    public function get_nmUser() {
-        return $this->_nama;
-    }
-
-    public function get_pass() {
-        return $this->_pass;
-    }
-
-    public function get_akses() {
-        return $this->_akses;
-    }
-
-    public function get_foto() {
-        return $this->_foto;
-    }
-
-    /*
-     * setter
-     */
-
-    public function set_id($id) {
-        $this->_id = $id;
-    }
-
     public function set_nip($nip) {
         $this->_nip = $nip;
+    }
+
+    public function get_nmUser() {
+        return $this->_nama;
     }
 
     public function set_nmUser($nama) {
         $this->_nama = $nama;
     }
 
+    public function get_pass() {
+        return $this->_pass;
+    }
+
     public function set_pass($pass) {
         $this->_pass = $pass;
     }
 
+    public function get_akses() {
+        return $this->_akses;
+    }
+
     public function set_akses($akses) {
         $this->_akses = $akses;
+    }
+
+    public function get_foto() {
+        return $this->_foto;
     }
 
     public function set_foto($foto) {
