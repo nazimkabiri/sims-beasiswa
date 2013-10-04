96d7d09f3fd42501547ede225fc7d7e221b9ad03
mashtree@gmail.com <mashtree@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-10-04 09:15:38 +0000

    revisi dari revisi sebelumnya

--- trunk/app/view/profil/display_spmt.php
+++ trunk/app/view/profil/display_spmt.php
@@ -3,7 +3,7 @@
 if($this->file!='' AND file_exists('files/spmt/'.$this->file)) {?>
 <div class="vlamp"><iframe  width= 800 height= 500 src="<?php echo URL;?>files/spmt/<?php echo $this->file;?>">
     <?php }else{
-        echo "</br></br></br></br></br><h2 align=center>File Transkrip Belum Ada</h2>";
+        echo "</br></br></br></br></br><h2 align=center>File SPMT Belum Ada</h2>";
     } ?>
     
     <p align="center">Mohon segera upload file surat yang bersangkutan</p>
