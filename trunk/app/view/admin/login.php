ebe16fa1f2c48694ba63d593e9b52e7af3190e20
mashtree@gmail.com <mashtree@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-10-10 04:02:56 +0000

    ngubah halaman login ae, sementara kopas darimana hayoo :D

--- trunk/app/view/admin/login.php
+++ trunk/app/view/admin/login.php
@@ -10,14 +10,15 @@
         <script src="<?php echo URL; ?>public/js/myjs.js"></script>
         <script src="<?php echo URL; ?>public/js/teamdf-jquery-number/jquery.number.js"></script>
         <link href="<?php echo URL; ?>public/css/style.css" rel="stylesheet">
+        <link href="<?php echo URL; ?>public/css/form.css" rel="stylesheet">
     </head>
     <body>
-        <div>
+<!--        <div>
             <div>LOGO</div>
             <div>
             <?php 
                 if(isset($this->error)){
-                    echo "<div style='color:red'><h2>".$this->error."<h2></div>";
+                    echo "<div style='color:red' id=notfound><h2>".$this->error."<h2></div>";
                 }
             ?>
             <form method="POST" action="<?php echo URL;?>auth/login">
@@ -28,21 +29,55 @@
                 <input type="reset" value="KESET"><input type="submit" value="LOGIN" onclick="return cek();">
             </form>
             </div>
-        </div>
+        </div>-->
+        <div class="login-container">
+            <a class="logo" href="/index.php"><img title="SIMS" height="64" src="<?php echo URL; ?>public/img/logo.png" alt="SIMS" /></a><div class="form login">
+            <?php 
+                if(isset($this->error)){
+                    echo "<div style='color:red' id=notfound><h2>".$this->error."<h2></div>";
+                }
+            ?>
+            <h1>Login</h1>
+            <form id="login-form" action="<?php echo URL;?>auth/login" method="post">	<div class="row">
+                            <label>Username <input name="user" id="nuser" type="text" /> </label>
+                            <div class="error" id="wuser" style="display:none"></div>	</div>
+                    <div class="row">
+                            <label>Password <input style="" name="pass" id="pass" type="password" /> </label>
+                                            <div class="error" id="wpass" style="display:none"></div>	</div>
+                    <div class="row buttons">
+                            <label> <input id="button" type="submit" name="yt0" value="Login" onClick="return cek()"/> </label>
+                    </div>
+            </form>
+            <div class="clearfix"></div>
+            <div class="copyright">
+                                            Scholarship Information Management System - Bag Pengembangan Pegawai Setditjen PBN.<br />
+                                            Copyright &copy; 2013 by <a class="djpbn" title="Direktorat Jenderal Perbendaharaan" href="http://www.perbendaharaan.go.id/">Direktorat Jenderal Perbendaharaan</a>.<br/>
+                                            All Rights Reserved.<br/>
+
+                                    </div>
+            </div>
+            </div>
     </body>
 </html>
 
 <script type="text/javascript">
 $(function(){
+    var notfound = document.getElementById('notfound');
     $('#nuser').focus();
     $('#wuser').fadeOut();
     $('#wpass').fadeOut();
     $('#nuser').keyup(function(){
+        if(notfound!=null){
+            $('#notfound').fadeOut(200);
+        }
         if($('#nuser').val()!=''){
             $('#wuser').fadeOut(200);
         }
     })
     $('#pass').keyup(function(){
+        if(notfound!=null){
+            $('#notfound').fadeOut(200);
+        }
         if($('#npass').val()!=''){
             $('#wpass').fadeOut(200);
         }
