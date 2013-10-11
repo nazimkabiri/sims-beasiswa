668ef0181fb9f0764bb9d2a613bb635de0d13386
asuracentaurix <asuracentaurix@gmail.com>
2013-10-11 08:10:53 +0000

    update ... memperbaiki view dari jadup, dan buku

--- app/view/bantuan/rekam_buku.php
+++ app/view/bantuan/rekam_buku.php
@@ -2,121 +2,126 @@
     BIAYA BUKU > TAMBAH <!-- memakai breadcrumb -->
 </div>
 <div>
-     <form method="POST" action="
-          <?php
-                echo URL.'elemenBeasiswa/viewUangBuku'
-          ?>
-          " enctype="multipart/form-data">
-    <?php 
-    if(isset($this->d_ubah)){
-        echo "<input type='hidden' name='kode_d' id='kode_d' value=".$this->d_ubah->get_kd_d().">";
-    }
-    ?>
-    <div>
-        <input type="hidden" id="kode_r" name="kode_r" size="12" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_kd_r():'2';?>">
-        <input type="hidden" id="jml_peg" name="jml_peg" size="12" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_jml_peg():'';?>">
+    <form method="POST" action="<?php
+echo URL;
+'elemenBeasiswa/viewUangBuku'
+?> " enctype="multipart/form-data">
+              <?php
+              if (isset($this->d_ubah)) {
+                  echo "<input type='hidden' name='kode_d' id='kode_d' value=" . $this->d_ubah->get_kd_d() . ">";
+              }
+              ?>
         <div>
-            <table>
-                <tr>
-                    <td>
-                        <label>Universitas : </label>
-                        <select id="universitas" name="universitas">
-                        <?php 
-                            foreach ($this->fakul as $val){
-                            echo "<option value=".$val->get_kode_fakul()." >".$val->get_kode_univ()." - ".$val->get_nama()."</option>";
-                            }
-                        ?> 
-                        </select>
-                        <label>Jurusan/Prodi : </label>
-                        <select>
-                        <?php 
-                            foreach ($this->jur as $val2){
-                                echo "<option value=".$val2->get_kode_jur()." >".$val2->get_nama()."</option>";
-                            }
-                        ?>
-                        </select>
-                        <label>Tahun Masuk : </label>
-                        <select>
-                        <?php 
-                            foreach ($this->kon as $val3){
-                                echo "<option value=".$val3->thn_masuk_kontrak." >".$val3->thn_masuk_kontrak."</option>";
-                            }
-                        ?>
-                        </select>
-                    </td>
-                    <td>
-                        <label>Semester dan Tahun : </label>
-                        <select id="bln" name="bln" >
-                            <option value="1">Semester I</option>
-                            <option value="2">Semester II</option>
-                        </select>
-                        <select id="thn" name="thn">
-                            <option value="2012">2012</option>
-                            <option value="2013">2013</option>
-                            <option value="2014">2014</option>
-                            <option value="2015">2015</option>
-                            <option value="2016">2016</option>
-                            <option value="2017">2017</option>
-                        </select>
-                        <label>Biaya Per Pegawai : </label>
-                        <input disable type="text" id="biaya_peg" name="biaya_peg" size="12" value="500.000">
-                        <label>Total Biaya : </label>
-                        <input type="text" id="total_bayar" name="total_bayar" size="12" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_total_bayar():'';?>">
-                    </td>
-                    <td>
-                        <label>No. SP2D : </label>
-                        <input type="text" id="no_sp2d" name="no_sp2d" size="20" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_no_sp2d():'';?>">
-                        <label>Tgl SP2D : </label>
-                        <input type="text" id="tgl_sp2d" name="tgl_sp2d" size="20" value="<?php echo isset($this->d_ubah)?$this->d_ubah->get_tgl_sp2d():'';?>">
-                        <label>File SP2D : </label>
-                        <input type="file" id="fupload" name="fupload" size="20" >
-                    </td>
-                </tr>
-            </table>
+            <input type="hidden" id="kode_r" name="kode_r" size="12" value="<?php echo isset($this->d_ubah) ? $this->d_ubah->get_kd_r() : '2'; ?>">
+            <input type="hidden" id="jml_peg" name="jml_peg" size="12" value="<?php echo isset($this->d_ubah) ? $this->d_ubah->get_jml_peg() : ''; ?>">
+            <fieldset>
+                <table>
+                    <tr>
+                        <td>
+                            <label>Universitas : </label>
+                            <select type="text" id="universitas" name="universitas">
+                                <?php
+                                foreach ($this->fakul as $val) {
+                                    echo "<option value=" . $val->get_kode_fakul() . " >" . $val->get_kode_univ() . " - " . $val->get_nama() . "</option>";
+                                }
+                                ?> 
+                            </select>
+                            <label>Jurusan/Prodi : </label>
+                            <select type="text" id="jurusan" name="jurusan">
+                                <?php
+                                foreach ($this->jur as $val2) {
+                                    echo "<option value=" . $val2->get_kode_jur() . " >" . $val2->get_nama() . "</option>";
+                                }
+                                ?>
+                            </select>
+                            <label>Tahun Masuk : </label>
+                            <select type="text" name="tahun_masuk">
+                                <?php
+                                foreach ($this->kon as $val3) {
+                                    echo "<option value=" . $val3->thn_masuk_kontrak . " >" . $val3->thn_masuk_kontrak . "</option>";
+                                }
+                                ?>
+                            </select>
+                        </td>
+                        <td>
+                            <label>Semester dan Tahun : </label>
+                            <select type="text" id="bln" name="bln" >
+                                <option value="1">Semester I</option>
+                                <option value="2">Semester II</option>
+                            </select>
+                            <select type="text" id="thn" name="thn">
+                                <option value="2012">2012</option>
+                                <option value="2013">2013</option>
+                                <option value="2014">2014</option>
+                                <option value="2015">2015</option>
+                                <option value="2016">2016</option>
+                                <option value="2017">2017</option>
+                            </select>
+                            <label>Biaya Per Pegawai : </label>
+                            <input disabled type="text" id="biaya_buku" name="biaya_buku" size="12" value="<?php echo $this->buku ?>"/>
+                            <label>Total Biaya : </label>
+                            <input type="text" id="total_bayar" name="total_bayar" size="12" value="<?php echo isset($this->d_ubah) ? $this->d_ubah->get_total_bayar() : ''; ?>">
+                        </td>
+                        <td>
+                            <label>No. SP2D : </label>
+                            <input type="text" id="no_sp2d" name="no_sp2d" size="20" value="<?php echo isset($this->d_ubah) ? $this->d_ubah->get_no_sp2d() : ''; ?>"/>
+                            <label>Tgl SP2D : </label>
+                            <input type="text" id="tgl_sp2d" name="tgl_sp2d" size="20" value="<?php echo isset($this->d_ubah) ? $this->d_ubah->get_tgl_sp2d() : ''; ?>"/>
+                            <label>File SP2D : </label>
+                            <input type="file" id="fupload" name="fupload" size="20" />
+                        </td>
+                    </tr>
+                </table>
+                <div>
+                    <input type="submit" name="simpan" value="simpan" class="sukses"/>
+                    <input type="reset" name="baral" value="batal" class="normal">
+                </div>
+            </fieldset>
         </div>
-    </div>
-</div>
-<div>
-    <div>Data Penerima Biaya Buku</div>
-    <div>file link print</div>
-</div>
-<div>
-    <table>
-        <thead>
-        <th>No</th>
-        <th>Nama / NIP</th>
-        <th>Gol</th>
-        <th>Status</th>
-        <th>Bank Penerima</th>
-        <th>No. Rekening</th>
-        <th>Pilih</th>
-        </thead>
-        <tbody>
-            <?php
-                $no = 1;
-                foreach ($this->pb as $val4){
-                    echo "<tr>";
-                    echo "<td>$no</td>";
-                    echo "<td>".$val4->get_nama()." / ".$val4->get_nip()."</td>";
-                    echo "<td>".$val4->get_gol()."</td>";
-                    echo "<td>".$val4->get_status()."</td>";
-                    echo "<td>".$val4->get_bank()."</td>";
-                    echo "<td>".$val4->get_no_rek()."</td>";
-            ?>
-                    <td><input type="check-box" id="setuju" name="setuju" size="4" value="harusnya dalam bentuk chckbox"></td>
-            <?php
+
+        <div>
+            <div>Data Penerima Biaya Buku</div>
+            <div>file link print</div>
+        </div>
+
+        <fieldset>
+            <table class="table-bordered zebra">
+                <thead>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Gol</th>
+                <th>Status</th>
+                <th>Bank Penerima</th>
+                <th>No. Rekening</th>
+                <th>Pilih</th>
+                </thead>
+                <tbody>
+                <?php
+                    $no = 1;
+                    foreach ($this->pb as $val4) {
+                        echo "<tr>";
+                        echo "<td>$no</td>";
+                        echo "<td>" . $val4->get_nama() . " / " . $val4->get_nip() . "</td>";
+                        echo "<td>" . $val4->get_gol() . "</td>";
+                        echo "<td>" . $val4->get_status() . "</td>";
+                        echo "<td>" . $val4->get_bank() . "</td>";
+                        echo "<td>" . $val4->get_no_rek() . "</td>";
+                        ?>
+                    <td><input type="checkbox" id="setuju" name="setuju" /></td>
+                    <?php
                     $no++;
                 }
-            ?>         
-        </tbody>
-    </table>
-</div>
-<div>
-    <div>Keterangan : *Data harus diisi</div>
-    <div>
-        <input class="normal" type="submit" onclick="" value="BATAL">
-        <input class="sukses" type="submit" name="<?php echo isset($this->d_ubah)?'update_elem':'add_elem';?>" value="SIMPAN" onClick="return cek();">
-    </div>
-</div>
+                ?>         
+                </tbody>
+            </table>
+        </fieldset>
+
+        <div>
+            <div>Keterangan : *Data harus diisi</div>
+            <div>
+                <input type="submit" name="simpan" value="simpan" class="sukses"/>
+                <input type="reset" name="baral" value="batal" class="normal">
+            </div>
+        </div>
     </form>
 </div>
