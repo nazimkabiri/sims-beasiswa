ebcfdc55199cd06377207238163da5352fca1854
mashtree@gmail.com <mashtree@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-10-25 03:06:36 +0000

    notifikasi

--- trunk/app/view/index.php
+++ trunk/app/view/index.php
@@ -40,7 +40,8 @@
                 margin-left: 10px;
         }
         ul.noti {
-                height: 75px; margin: 5px; 
+                height: 75px; margin: 5px;
+                width:100%;
                 background-color: #F3F3F3;
                 background-image: url('public/img/bg.png');
                 -webkit-border-radius: 40px 0px 0px 40px;
@@ -86,65 +87,71 @@
 <script type="text/javascript">
     $(function(){
         var data_notif = <?php echo $this->d_notif; ?>;
-        var length_data = data_notif.length;
-//        console.log(length_data);
-        var max_data = 10;
-        var num_div = Math.ceil(length_data/max_data);
+        var length_data = data_notif.length; //amount of data
+        var max_data = 7; //max data per page
+        var time_per_item = 10; //on second
+//        console.log(max_data);
+        var num_div = Math.ceil(length_data/max_data); //num of page
 //        console.log(num_div);
         if(length_data>0){
             document.getElementById('beranda').className = 'notif';
+            var notif_container = document.createElement('div');
+            notif_container.className = 'marquee';
             var ul = document.createElement('div');
-//            ul.className = 'marquee'
             var container;
             for(var i=0;i<length_data;i++){
-                var jinah = i%10;
-//                console.log('p:nth-child('+(jinah+1)+')')
+                var jinah = i%max_data;
                 
                 if((jinah)==0){
-                    container = document.createElement('p');
-//                    var child = Math.ceil(i/10);
-////                    console.log('p:nth-child('+child+')');
-//                    var order = num_to_text(Math.ceil(i/10));
-//                    $('p:nth-child('+child+')').css({'animation':'left-'+order+' 10s ease infinite','-moz-animation':'left-one 10s ease infinite','-webkit-animation':'left-one 10s ease infinite'}); //nambah css ke p child ke n         
+                    container = document.createElement('p');   
                 }
                 var li = document.createElement('ul');
-//                li.className = 'jadup';
-                //                var content = data_notif[i].jenis;
                 //                li.appendChild(document.createTextNode(content));
                 var jenis = '';
-                var judul_notif = '';
-                var pesan = '';
-                var jatuh_tempo = (data_notif[i].bulan).substr(0,3)+" "+data_notif[i].tahun
+                var judul_notif = ''; //judul notifikasi
+                var pesan = ''; //detail pesan notifikasi
+                var jatuh_tempo = (data_notif[i].bulan).substr(0,3)+" "+data_notif[i].tahun 
+                var bul_sem = data_notif[i].jatuh_tempo; //bulan untuk semester
                 switch(data_notif[i].jenis){
                     case 'jadup':
                         li.className = 'inline noti';
-                        jenis = 'JADUP '
-                        judul_notif = 'Tunjangan Hidup'
-                        pesan = jenis+" "+data_notif[i].univ+" jur. "+data_notif[i].jurusan+" Tahun masuk "+data_notif[i].tahun_masuk+" dalam "+data_notif[i].status+" oleh "+data_notif[i].nama_pic;
+                        jenis = 'Pembayaran Jadup '
+                        judul_notif = 'JADUP'
+                        pesan = jenis+" "+data_notif[i].univ+" - "+data_notif[i].jurusan+" "+data_notif[i].tahun_masuk+" bulan "+data_notif[i].bulan+" "+data_notif[i].tahun;
                         break;
                     case 'buku':
+                        //bagian ini belum selesai
+                        //masih terkendala dengan penentuan jatuh tempo pembayaran
+                        //kapan trigger notif dimunculkan
+                        var tmp = data_notif[i].jatuh_tempo;
+//                        console.log(tmp)
+                        var bul_sem = tmp.substr(4,2);
+//                        console.log(bul_sem);
                         li.className = 'inline noti';
-                        jenis = 'UANG BUKU '
-                        judul_notif = 'Bantuan Buku'
-                        pesan = jenis+" "+data_notif[i].univ+" jur. "+data_notif[i].jurusan+" Tahun masuk "+data_notif[i].tahun_masuk+" dalam "+data_notif[i].status+" oleh "+data_notif[i].nama_pic;
+                        jenis = 'Pembayaran Uang Buku '
+                        judul_notif = 'UANG BUKU'
+                        pesan = jenis+" "+data_notif[i].univ+" - "+data_notif[i].jurusan+" "+data_notif[i].tahun_masuk+" untuk semester "+data_notif[i].bulan+" "+data_notif[i].tahun;
                         break;
                     case 'skripsi':
+                        var tmp = data_notif[i].jatuh_tempo;
+                        console.log(tmp)
                         li.className = 'inline noti';
-                        jenis = 'UANG SKRIPSI '
-                        judul_notif = 'Bantuan Skripsi'
-                        pesan = jenis+" "+data_notif[i].univ+" jur. "+data_notif[i].jurusan+" Tahun masuk "+data_notif[i].tahun_masuk+" dalam "+data_notif[i].status+" oleh "+data_notif[i].nama_pic;
+                        jenis = 'Pembayaran Uang Skripsi '
+                        judul_notif = 'UANG SKRIPSI'
+                        pesan = jenis+" "+data_notif[i].univ+" - "+data_notif[i].jurusan+" "+data_notif[i].tahun_masuk+" bulan "+data_notif[i].bulan+" "+data_notif[i].tahun;
                         break;
                     case 'lulus':
                         li.className = 'inline noti';
                         jenis = 'Pegawai TB dari '
-                        judul_notif = 'Masa Tugas Belajar'
-                        pesan = jenis+" "+data_notif[i].univ+" jur. "+data_notif[i].jurusan+" Tahun masuk "+data_notif[i].tahun_masuk+" : "+ data_notif[i].nama_pic;
+                        judul_notif = 'MASA TUGAS BELAJAR'
+                        pesan = jenis+" "+data_notif[i].univ+" - "+data_notif[i].jurusan+" "+data_notif[i].tahun_masuk+" selesai bulan "+data_notif[i].bulan+" "+data_notif[i].tahun;
                         break;
                     case 'kontrak':
                         li.className = 'inline noti';
-                        jenis = 'TAGIHAN '
-                        judul_notif = 'Pembayaran Kontrak'
-                        pesan = jenis+" "+data_notif[i].univ+" jur. "+data_notif[i].jurusan+" Tahun masuk "+data_notif[i].tahun_masuk+" dalam "+data_notif[i].status+" oleh "+data_notif[i].nama_pic;
+                        jenis = 'Tagihan Kontrak '
+                        judul_notif = 'PEMBAYARAN KONTRAK'
+                        pesan = jenis+" "+data_notif[i].univ+" - "+data_notif[i].jurusan+" "+data_notif[i].tahun_masuk+" bulan "+data_notif[i].bulan+" "+data_notif[i].tahun;
+                        console.log(pesan)
                         break;
                 }
                 judul_notif.toUpperCase();
@@ -160,7 +167,7 @@
                 
                 a.style.color = '#49afcd'
                 a.appendChild(document.createTextNode(judul_notif));
-                h4.appendChild(document.createTextNode(jatuh_tempo+':'))
+                h4.appendChild(document.createTextNode(jatuh_tempo+' : '))
                 h4.appendChild(a);
                 
                 tgl.appendChild(document.createTextNode(jatuh_tempo));
@@ -170,13 +177,12 @@
                 var p = document.createElement('div');
                 p.className= 'noti'
                 p.appendChild(document.createTextNode(pesan));
-                //                content_notif.appendChild(judul)
                 content_notif.appendChild(h4);
                 content_notif.appendChild(p);
                 
                 img.className='noti'
                 var status_proses = data_notif[i].status==='proses'
-                console.log(status_proses); 
+//                console.log(status_proses); 
                 if(status_proses){
                     img.src = 'public/icon/purges.png'
                 }else{
@@ -185,41 +191,34 @@
                 
                 flag.appendChild(img);
                 
-//                tgl.className = "tanggal";
                 content_notif.className = "detail";
                 pic.className = 'fotopic';
                 flag.className = 'flag'
                 
-//                content_notif.appendChild(tgl);
                 li.appendChild(pic);
                 li.appendChild(content_notif);
                 li.appendChild(flag);
                 
                 container.appendChild(li);
                 ul.appendChild(container);
-                //                console.log(data_notif[i])
             }
-            document.getElementById('beranda').appendChild(ul);
+            notif_container.appendChild(ul);
+            document.getElementById('beranda').appendChild(notif_container);
         }
         
         for(var i=1;i<=num_div;i++){
-            var order = num_to_text(i)+'';
-//            console.log(order+' '+i);
-            var num = i+'';
-            var timing = num_div*3; //ntar diganti 10
+            var order = num_to_text(i)+''; //untuk penamaan animasi
+            var num = i+''; //child ke-
+            var timing = num_div*time_per_item; //ntar diganti 10
             var animation = 'left-'+order+' 20s ease infinite';
             $('p:nth-child('+num+')').css({'animation':'left-'+order+' '+timing+'s ease infinite','-moz-animation':'left-'+order+' '+timing+'s ease infinite','-webkit-animation':'left-'+order+' '+timing+'s ease infinite'}); //nambah css ke p child ke n         
         }
         
-//        for(i=0;i<21;i++){
-//            num_to_text(i);
-//        }
         add_keyframe(num_div);
     });
     
     var add_keyframe = function(num_elemen){
-        var div = split_persen(num_elemen);
-//        console.log(div)
+        var div = split_persen(num_elemen); 
         var cssAnimation = document.createElement('style');
         cssAnimation.type = 'text/css';
         var persen = 0;
@@ -228,16 +227,21 @@
             var order = num_to_text(i+1);
             var frame_one = i*div;
             var frame_two = frame_one+step_frame; //mulai bergerak
-            var frame_four = (i+1)*div;
-            var frame_three = frame_four-step_frame; 
+            if((num_elemen-1)==i){
+                var frame_four = 100;
+            }else{
+                var frame_four = (i+1)*div;
+            }
+            
+            var frame_three = frame_four-step_frame;
 //            console.log(frame_one+' '+frame_two+' '+frame_three+' '+frame_four)
             var rules = document.createTextNode('@-webkit-keyframes left-'+order+' {'+
             '0% { -webkit-transform:translateX(100%); }'+
             ''+frame_one+'% { -webkit-transform:translateX(100%); }'+
             ''+frame_two+'% { -webkit-transform:translateX(0); }'+
             ''+frame_three+'% { -webkit-transform:translateX(0); }'+
-            ''+frame_four+'% { -webkit-transform:translateX(-100%); }'+
-            '100% { -webkit-transform:translateX(-100%); }'+
+            ''+frame_four+'% { -webkit-transform:translateX(-110%); }'+
+            '100% { -webkit-transform:translateX(-110%); }'+
             '}'+
             '@-moz-keyframes left-'+order+' {'+
             '0% { left:100%; }'+
@@ -247,14 +251,14 @@
             ''+frame_four+'% { left:-100; }'+
             '100% { left:-100%; }'+
             '}');
-            console.log(rules);
+//            console.log(rules);
             cssAnimation.appendChild(rules);
         }
         
         document.getElementsByTagName("head")[0].appendChild(cssAnimation);
     }
     
-    var split_persen = function(num){
+    var split_persen = function(num){ 
         return Math.floor(100/num);
     }
     
@@ -312,7 +316,6 @@
     }
 
     var num_ten_to_twenty = function(num){
-//            var len = (num.toString()).length;
         if(num>20 || num<10){
             alert('angka harus antara 10 sampai 20');
         }
@@ -349,6 +352,14 @@
             return 'fif'+ten+' '+num_below_ten(denomination)
         }else{
             return num_below_ten(tens)+ty+' '+num_below_ten(denomination)
+        }
+    }
+    
+    var get_semester = function(bulan){
+        if(bulan<8){
+            return 'genap';
+        }else{
+            return 'ganjil';
         }
     }
 
