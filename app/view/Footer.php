d59dc42e0a209fe781f655a671354db187d8d9cf
nez817 <nez817@gmail.com>
2013-10-29 07:25:28 +0000

    view footer v2, kontrak v5

--- app/view/Footer.php
+++ app/view/Footer.php
@@ -1,17 +1,13 @@
-</div>
 </body>
-<hr>
-<style>
-	
-</style>
-<footer>&copy SIMS. Aplikasi ini dikembangkan oleh <a href="#openModal">Djepit5</a>
+<footer>&copy 2013 SIMS. Pengembangan Pegawai - <a href="http://www.perbendaharaan.go.id">Dirjen Perbendaharaan</a>. <a href="#openModal">Developer</a>.
 <div id="openModal" class="modalDialog">
 	<div>
 		<a href="#close" title="Tutup" class="close"><i class="icon-remove icon-white" style="margin-left: 4px; margin-top: 1px"></i></a>
 		<h2>Tim Djepit5: </h2>
-		<center><p><a href="mailto:mashtree@gmail.com" target="_top">Triyono</a>, <a href="mailto:fromjayakarta@gmail.com" target="_top">Slamet Darmawan</a>, <a href="mailto:asuracentaurix@gmail.com" target="_top">Candra Dwi Aprida</a>, <a href="mailto:andi.saputra.jakarta@gmail.com" target="_top">Andi Saputra</a>, <a href="mailto:nez817@gmail.com" target="_top">Ernestina Er</a>.</p></center>
+		<center><p><a href="mailto:mashtree@gmail.com" target="_top">Triyono</a> || <a href="mailto:andi.saputra.jakarta@gmail.com" target="_top">Andi Saputra</a> || <a href="mailto:asuracentaurix@gmail.com" target="_top">Candra Dwi Aprida</a> || <a href="mailto:fromjayakarta@gmail.com" target="_top">Slamet Darmawan</a> || <a href="mailto:nez817@gmail.com" target="_top">Ernestina R</a>.</p></center>
 		
 	</div>
+</div>
 </div>
 </footer>
 </html>
