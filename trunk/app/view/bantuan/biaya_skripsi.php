2b1dea39f84eefe600e600220bbdb9dfe9a6503e
fromjayakarta@gmail.com <fromjayakarta@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-11-12 10:41:55 +0000

    - kasih blink di skripsi

--- trunk/app/view/bantuan/biaya_skripsi.php
+++ trunk/app/view/bantuan/biaya_skripsi.php
@@ -124,6 +124,16 @@
     })
 	
 	$(".hover").css("cursor","pointer"); 
+	
+	function blink(selector){
+	$(selector).fadeOut('slow', function(){
+    $(this).fadeIn('slow', function(){
+        blink(this);
+    });
+	});
+	}
+
+	blink('.hover');
     
     
 </script>
