dbdd585148b4aa2b4cee6d42c0fde5c34659952e
mashtree@gmail.com <mashtree@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-11-18 09:44:21 +0000

    revisi

---
+++ trunk/app/view/index_1.php
@@ -0,0 +1,109 @@
+<?php if($this->is_pic){ ?>
+<div id="beranda" style="margin: 20px 20px 5px 20px; border: solid 1px;width:70%;display:inline-block; float:left">
+<?php
+    foreach ($this->d_notif as $v){
+        switch($v['jenis']){
+            case 'jadup':
+                $jenis = 'Pembayaran Jadup ';
+                $judul_notif = 'JADUP';
+                $pesan = $jenis." ".$v['univ']." - ".$v['jurusan']." ".$v['tahun_masuk']." bulan ".$v['bulan']." ".$v['tahun'];
+                break;
+            case 'buku':
+                $tmp = $v['jatuh_tempo'];
+                $jenis = 'Pembayaran Uang Buku ';
+                $judul_notif = 'UANG BUKU';
+                $sem = (int) Tanggal::bulan_num($v['bulan']);
+                if($bln==9){
+                    $sem = 'ganjil';
+                }else{
+                    $sem = 'genap';
+                }
+                $pesan = $jenis." ".$v['univ']." - ".$v['jurusan']." ".$v['tahun_masuk']." untuk semester ".$sem." ".$v['tahun'];
+                break;
+            case 'skripsi':
+                $jenis = 'Pembayaran Uang Skripsi ';
+                $judul_notif = 'UANG SKRIPSI';
+                $pesan = $jenis." ".$v['univ']." - ".$v['jurusan']." ".$v['tahun_masuk'];
+                break;
+            case 'lulus':
+                $jenis = 'Pegawai TB dari ';
+                $judul_notif = 'MASA TUGAS BELAJAR';
+                $pesan = $jenis." ".$v['univ']." - ".$v['jurusan']." ".$v['tahun_masuk']." selesai bulan ".$v['bulan']." ".$v['tahun'];
+                break;
+            case 'kontrak':
+                $jenis = 'Tagihan Kontrak ';
+                $judul_notif = 'PEMBAYARAN KONTRAK';
+                $pesan = $jenis." ".$v['univ']." - ".$v['jurusan']." ".$v['tahun_masuk']." bulan ".$v['bulan']." ".$v['tahun'];
+                break;
+        }
+        $jatuh_tempo = "01 ".$v['bulan']." ".$v['tahun'];
+        $tmp = explode('-',$v['jatuh_tempo']);
+        if(count($tmp)>2){
+            $jatuh_tempo = Tanggal::tgl_indo($v['jatuh_tempo']);
+        }
+        
+        if($v['status']=='proses'){
+            $img = 'public/icon/purges.png';
+        }else{
+            $img = 'public/icon/notices.png';
+        }
+        echo "<ul class='inline noti pic'>";
+        echo "<div class='detail'>";
+        echo "<h4>$jatuh_tempo : <a style='color: #49afcd' href=''>$judul_notif</a></h4>";
+        echo "<div class='noti'>$pesan</div>";
+        echo "</div>";
+        echo "<div class='flag'><img class='noti pic' src='$img'></div></ul>";
+
+    }
+?>
+    
+</div>
+<div style="float:left; width:24%; margin: 20px 0% 5px 0px;border: solid 1px">
+    <div><img class="frame" src="files/foto/<?php echo $this->d_user->get_foto();?>" style="width:80%;margin: 5px 8% 5px 8%;"></div>
+    <div><h4><?php echo strtoupper($this->d_user->get_nmUser());?></h4></div>
+    <div><h4>Universitas :</h4>
+        <div style="margin:10px 5px 10px 10px">
+        <?php 
+            foreach ($this->d_univ as $univ){
+                echo $univ[0]." ".$univ[1]." pegawai</br>";
+            }
+        ?>
+        </div>
+    </div>
+    
+</div>
+<?php } ?>
+<input type="hidden" id="jml_notif" value="<?php echo $this->count_notif;?>">
+<script type="text/javascript">
+
+$(function(){
+//    display_notif();
+    var time = 5*1000;
+    setInterval(function(){
+        count_notif();
+    },time);
+})
+
+function count_notif(){
+    $.post('<?php echo URL; ?>notifikasi/count_notif',{param:""+<?php echo Session::get('kd_user');?>+""},
+    function(data){
+        var old_count = parseInt(document.getElementById('jml_notif').value);
+        if(parseInt(data)>old_count){
+            console.log(data);
+            $('#jml_notif').val(data);
+            $('<audio id="chatAudio"><source src="public/sound/sounds-847-office-2.mp3" type="audio/mpeg"></audio>').appendTo('body');
+            $('#chatAudio')[0].play();
+//            document.getElementById("sound").innerHTML="<embed src='"+'public/sound/sounds-847-office-2.mp3'+"' hidden=true autostart=true loop=false>";
+            display_notif();
+        }
+    })
+}
+
+function display_notif(){
+    $.post('<?php echo URL;?>notifikasi/display_notif_pic',{param:""+<?php echo Session::get('kd_user');?>+""},
+    function(data){
+        $('#beranda').fadeIn(200);
+        $('#beranda').html(data);
+    })
+}
+</script>
---
+++ trunk/app/view/riwayat_tb/cetak_daftar_penerima.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>monitoring_kontrak_<?php echo Tanggal::getTimeSekarang(); ?></title>   
+        <style>
+            td, th {
+                border: 1px solid black;
+            }
+            table {
+                border-collapse: collapse;
+            }
+            .td2{
+                border: 0px ;
+            }
+
+            @media print {
+                #printbtn {
+                    display :  none;
+                }
+            }
+            </style>
+    </head>
+    <body style="font-family:arial;color:black;font-size:10px;">
+        <p align="center" style="font-weight: bold; font-size:13px;">
+            MONITORING KONTRAK KERJASAMA <br />
+            BEASISWA INTERNAL DIREKTORAT JENDERAL PERBENDAHARAAN <br />
+            <?php
+            if ($this->univ != "") {
+                echo strtoupper($this->data_univ->get_nama()) . "<br />";
+            }
+            if ($this->status != "") {
+                echo strtoupper($this->status) ." DIBAYAR<br />";
+            }
+            
+            if ($this->jadwal != "") {
+                echo "JADWAL PEMBAYARAN TAHUN " . strtoupper($this->jadwal) . "<br />";
+            }
+            
+            echo "PER ".strtoupper(Tanggal::getTglSekarangIndo());
+            echo "<br />";
+            echo "(PIC: ".strtoupper(Session::get('user')).")";
+            ?>
+
+        </p>
+         <table border="0" align="center" cellspacing=0 cellpadding=0 width=97% style="border-width: 0px; font-size: 10px;">
+            <tr>
+                <td class="td2" align="right"> 
+                    <FORM>
+                        <button TYPE="button" id="printbtn" onClick="cetak();">Cetak</button>
+                    </FORM>
+                </td>
+            </tr>
+        </table>
+        <br />
+        <table align="center" cellspacing=0 cellpadding=4 width=95% style="border-width: 1px; font-size: 10px; border-style: solid; border-color: black;">
+            <thead bgcolor="#E6F9ED">
+            <th>No</th>
+            <th>No & Tgl Kontrak</th>
+            <th>Jurusan</th>
+            <th>Nama Biaya</th>
+            <th>Jumlah Biaya</th>
+            <th>Jadwal <br />dibayarkan</th>
+<!--            <th>Jumlah dibayarkan</th>-->
+            <th>Status <br />Pembayaran</th>
+            <th>No dan Tgl SP2D</th>
+        </thead>
+
+        <?php
+        $i = 1;
+        $total = 0;
+        foreach ($this->data_biaya as $val) {
+            $data_kontrak = $this->kontrak->get_by_id($val->kd_kontrak);
+            //var_dump($data_kontrak);
+            //echo $data_kontrak->kd_jurusan;
+            $this->jurusan->set_kode_jur($data_kontrak->kd_jurusan);
+            $data_jurusan = $this->jurusan->get_jur_by_id($this->jurusan);
+            $data_universitas = $this->universitas->get_univ_by_jur($data_kontrak->kd_jurusan);
+            ?>
+            <tr>
+                <td><?php echo $i; ?></td>
+                <td><?php echo $data_kontrak->no_kontrak . " (" . $data_kontrak->tgl_kontrak . ")"; ?></td>
+                <td><?php echo $data_jurusan->get_nama() . " " . $data_universitas->get_kode() . " " . $data_kontrak->thn_masuk_kontrak; ?></td>
+                <td><?php echo $val->nama_biaya; ?></td>
+                <td align="right"><?php echo number_format($val->jml_biaya); ?></td>
+                <td><?php echo $val->jadwal_bayar; ?></td>
+<!--                <td><?php echo number_format($this->biaya->get_biaya_by_kontrak_dibayar($val->kd_kontrak)); ?></td>-->
+                <td>
+                    <?php echo $val->status_bayar; ?>
+                </td>
+                <td>
+                    <?php
+                    if ($val->tgl_sp2d != "01-01-1970") {
+                        echo $val->no_sp2d . " (" . $val->tgl_sp2d . ")";
+                    } else {
+                        echo "-";
+                    }
+                    ?>
+                </td>
+            </tr>
+            <?php
+            $i++;
+            $total = $total + $val->jml_biaya;
+        }
+        ?>
+            <tr>
+                <td colspan="4"></td>
+                <td align="right"><?php echo number_format($total); ?></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+    </table>
+</body>
+</html>
+<script type="text/javascript">
+    function cetak(){
+		window.print();
+		window.onfocus = function() { window.close(); }
+	}
+</script>
