ea43ea7de1c7fd572e1c798ca9899ffe02cc794a
fromjayakarta <fromjayakarta@gmail.com>
2013-11-26 07:10:24 +0000

    menambahkan lihat file/dokumen pada biaya.....

--- app/view/kontrak/data_biaya.php
+++ app/view/kontrak/data_biaya.php
@@ -38,11 +38,7 @@
             <th width="5%">No SP2D</th>
             <th width="5%">Tgl SP2D</th>
             <th width="5%">Status <br />Pembayaran</th>
-			<?php 
-			if(Session::get('role')==2){
-			?>
-            <th width="7%">Aksi</th>
-			<?php } ?>
+			<th width="7%"><?php if(Session::get('role')==2){ echo "Aksi";} else { echo "Dokumen/file";} ?></th>
             </thead>
 		<tbody style="text-align: center">
 
@@ -66,14 +62,64 @@
             }
                 ?></td>
                     <td><?php echo $val->status_bayar; ?></td>
+					
+                    <td>
 					<?php 
 					if(Session::get('role')==2){
 					?>
-                    <td>
                         <a href="<?php echo URL . "kontrak/delBiaya/" . $val->kd_biaya; ?>" onClick="return del('<?php echo $val->nama_biaya; ?>');" title="hapus"><i class="icon-trash"></i></a> &nbsp
                         <a href="<?php echo URL . "kontrak/editBiaya/" . $val->kd_biaya; ?> " title="ubah"><i class="icon-pencil"></i></a>
-                    </td>
+                    <?php } else { 
+					
+						//file bast
+						$file_bast = $val->file_bast;
+						if ($file_bast != "") {
+							$file_bast_ = $file_bast;
+						} else {
+							$file_bast_ = "";
+						}
+											
+						//file bap
+						$file_bap = $val->file_bap;
+						if ($file_bap != "") {
+							$file_bap_ = $file_bap;
+						} else {
+							$file_bap_ = "";
+						}
+						
+						//file ringkasan kontrak
+						$file_ring_kontrak = $val->file_ring_kontrak;
+						if ($file_ring_kontrak != "") {
+							$file_ring_kontrak_ = $file_ring_kontrak;
+						} else {
+							$file_ring_kontrak_ = "";
+						}
+						
+						//file kuitansi
+						$file_kuitansi = $val->file_kuitansi;
+						if ($file_kuitansi != "") {
+							$file_kuitansi_ = $file_kuitansi;
+						} else {
+							$file_kuitansi_ = "";
+						}
+						
+						//file sp2d
+						$file_sp2d = $val->file_sp2d;
+						if ($file_sp2d != "") {
+							$file_sp2d_ = $file_sp2d;
+						} else {
+							$file_sp2d_ = "";
+						}
+						?>
+						
+						<a href="<?php echo URL . "kontrak/fileBast/" . $file_bast_; ?>"target="file_bast" onClick="cetak_dokumen('file_bast');" title="BAST"><?php if ($file_bast_ != "") echo "BS"; ?></a>
+						<a href="<?php echo URL . "kontrak/fileBap/" . $file_bap_; ?>"target="file_bap" onClick="cetak_dokumen('file_bap');" title="BAP"><?php if ($file_bap_ != "") echo "BP"; ?></a>
+						<a href="<?php echo URL . "kontrak/fileRingKontrak/" . $file_ring_kontrak_; ?>"target="file_ring" onClick="cetak_dokumen('file_ring');" title="ringkasan Kontrak"><?php if ($file_ring_kontrak_ != "") echo "RK"; ?></a>
+						<a href="<?php echo URL . "kontrak/fileKuitansi/" . $file_kuitansi_; ?>"target="file_kuitansi" onClick="cetak_dokumen('file_kuitansi');" title="Kwitansi"><?php if ($file_kuitansi_ != "") echo "KW"; ?></a>
+						<a href="<?php echo URL . "kontrak/fileSp2d/" . $file_sp2d_; ?>"target="file_sp2d" onClick="cetak_dokumen('file_sp2d');" title="SP2D"><?php if ($file_sp2d_ != "") echo "SP"; ?></a>
 					<?php } ?>
+					</td>
+					
                 </tr>
                 <?php
                 $i++;
