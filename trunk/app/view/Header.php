3c47cd67818f0f0386d4749321af05c35c77e351
andi.saputra.jakarta@gmail.com <andi.saputra.jakarta@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-11-28 04:57:40 +0000

    hasil akhir dari aplikasi treascho beta 1.0

--- trunk/app/view/Header.php
+++ trunk/app/view/Header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>.:Treascho:.</title>   
+        <title>.:Treascho beta.1.0:.</title>   
         <script src="<?php echo URL; ?>public/js/jquery-2.0.3.min.js"></script>
 		<link rel="icon" type="image/png" href="<?php echo URL; ?>public/img/treascho-ico.png">
         <link rel="stylesheet" href="<?php echo URL; ?>public/js/jquery-ui-1.10.3/themes/base/jquery.ui.all.css">
