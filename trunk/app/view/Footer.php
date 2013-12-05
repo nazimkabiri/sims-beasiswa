65d1d8865f655755cc248e60ae17a307761a1a0f
andi.saputra.jakarta@gmail.com <andi.saputra.jakarta@gmail.com@c80b1f62-91eb-8b11-e5c1-14a3eefc285b>
2013-12-05 03:33:12 +0000

    finish

--- trunk/app/view/Footer.php
+++ trunk/app/view/Footer.php
@@ -1,12 +1,17 @@
 </div>
 </body>
 
-<footer>Scholarship Management and Early Warning System beta.1.0</br>&copy 2013 Bagian Pengembangan Pegawai - <a href="http://www.perbendaharaan.go.id">Ditjen Perbendaharaan</a><div class="profil2" style="border: no-border"><a href="#openModal">Developer</a></div>
-<div id="openModal" class="modalDialog">
+<footer>Scholarship Management and Early Warning System beta.1.0</br>Bagian Pengembangan Pegawai - <a href="http://www.perbendaharaan.go.id">Ditjen Perbendaharaan</a><div class="profil2" style="border: no-border">&copy 2013 <a href="#openModal">Developer</a></div>
+<div id="openModal" class="modalDialog" >
 	<div>
 		<h2>Developer :</h2>
 		<a href="#close" title="Tutup" class="close"><i class="icon-remove icon-white" style="margin-left: 4px; margin-top: 1px"></i></a>
-		<center><p><a href="https://www.facebook.com/saputra.andi" target="_top">Andi Saputra</a> || <a href="mailto:mashtree@gmail.com" target="_top">Triyono</a> || <a href="mailto:asuracentaurix@gmail.com" target="_top">Candra Dwi Aprida</a> || <a href="mailto:fromjayakarta@gmail.com" target="_top">Slamet Darmawan</a> || <a href="mailto:nez817@gmail.com" target="_top">Ernestina R</a>.</p></center>
+		<p>
+		<a href="mailto:andi.saputra.jakarta@gmail.com" target="_top">1. Andi Saputra</a> - 19850911200710001 - KPPN Tanjung Pinang </br> 
+		<a href="mailto:mashtree@gmail.com" target="_top">2. Triyono</a> -  198205152001121001 - AKKP Setditjen</br>  
+		<a href="mailto:asuracentaurix@gmail.com" target="_top">3. Candra Dwi Aprida</a> -  198404162006021007 - KPPN Batam</br> 
+		<a href="mailto:fromjayakarta@gmail.com" target="_top">4. Slamet Darmawan</a> - 198410232007101001 - Mutasi Kepegawaian Setditjen</br>  
+		<a href="mailto:nez817@gmail.com" target="_top">5. Ernestina R</a> - 198401282003122001 - Kanwil Jawa Tengah</p>
 		
 	</div>
 </div>
